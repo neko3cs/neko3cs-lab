e964d432e6dfd927e2b01c2149914c4238826f3c
neko3cs <minaton.0722.net@gmail.com>
2018-05-14 00:32:33 +0900

    #なんか競合した分がうまく変更できなかった...

--- SampleMailer/SampleMailer/ViewModels/SettingsPageViewModel.cs
+++ SampleMailer/SampleMailer/ViewModels/SettingsPageViewModel.cs
@@ -41,28 +41,9 @@ namespace SampleMailer.ViewModels
                 return;
             }
 
-
-            if (App.Account == null)
-            {
-                App.Account = new UserAccount()
-                {
-                    ID = 0,
-                    Name = Name,
-                    Address = Address,
-                    Password = Password,
-                    CreatedAt = DateTime.Now,
-                    UpdatedAt = null
-                };
-            }
-            else
-            {
-                App.Account.Name = Name;
-                App.Account.Address = Address;
-                App.Account.Password = Password;
-                App.Account.UpdatedAt = DateTime.Now;
-            }
-
-            //AccountRepo.SaveUserAccount(Account);
+            App.Account.Name = Name;
+            App.Account.Address = Address;
+            App.Account.Password = Password;
         });
     }
 }
