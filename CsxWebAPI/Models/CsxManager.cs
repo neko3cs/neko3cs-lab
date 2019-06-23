75db20079dc6c6a8d5361c4a1c29dd2532017b22
neko3cs <minaton.0722.net@gmail.com>
2019-06-23 12:21:44 +0900

    初期コミット

---
+++ CsxWebAPI/Models/CsxManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+namespace CsxWebAPI.Models
+{
+    public static class CsxManager
+    {
+        public static string WebRootPath { get; set; }
+
+        public static async Task<T> RunScriptAsync<T>(string scriptId)
+        {
+            var settings = AppSettings.Instance;
+
+            var scriptPath = $"{WebRootPath}{settings.ScriptPaths[scriptId]}";
+            var scriptText = File.ReadAllText(scriptPath, Encoding.UTF8);
+
+            var script = CSharpScript.Create<T>(scriptText);
+
+            var result = await script.RunAsync();
+            return result.ReturnValue;
+        }
+
+        public static async Task<T> RunScriptAsync<T>(string scriptId, dynamic args)
+        {
+            var settings = AppSettings.Instance;
+
+            var scriptPath = $"{WebRootPath}{settings.ScriptPaths[scriptId]}";
+            var scriptText = File.ReadAllText(scriptPath, Encoding.UTF8);
+
+            var script = CSharpScript.Create<T>(scriptText);
+
+            var result = await script.RunAsync(args);
+            return result.ReturnValue;
+        }
+    }
+}
