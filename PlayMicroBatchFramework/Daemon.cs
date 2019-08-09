740e795797c766926589832c957136e39c6bce8e
neko3cs <minaton.0722.net@gmail.com>
2019-08-10 01:53:03 +0900

    add: Daemonの追加

---
+++ PlayMicroBatchFramework/Daemon.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using MicroBatchFramework;
+using Microsoft.Extensions.Logging;
+
+namespace PlayMicroBatchFramework
+{
+    public class Daemon : BatchBase
+    {
+        public async Task Run()
+        {
+            try
+            {
+                while (!Context.CancellationToken.IsCancellationRequested)
+                {
+                    try
+                    {
+                        Context.Logger.LogDebug("Wait One Minutes");
+                    }
+                    catch (Exception ex)
+                    {
+                        Context.Logger.LogError(ex, "Found error");
+                    }
+
+                    await Task.Delay(TimeSpan.FromMinutes(1), Context.CancellationToken);
+                }
+            }
+            catch (Exception ex) when (!(ex is OperationCanceledException))
+            {
+                Context.Logger.LogError(ex, "Exception throws!");
+            }
+        }
+    }
+}
