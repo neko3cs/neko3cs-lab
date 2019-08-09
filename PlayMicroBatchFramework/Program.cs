3ab68e3942e1e03bf026dd0355f37ace373651d8
neko3cs <minaton.0722.net@gmail.com>
2019-08-10 01:53:03 +0900

    add: Daemonの追加

--- PlayMicroBatchFramework/Program.cs
+++ PlayMicroBatchFramework/Program.cs
@@ -6,6 +6,6 @@ namespace PlayMicroBatchFramework
     class Program
     {
         static async Task Main(string[] args)
-            => await BatchHost.CreateDefaultBuilder().RunBatchEngineAsync<MyFirstBatch>(args);
+            => await BatchHost.CreateDefaultBuilder().RunBatchEngineAsync(args);
     }
 }
