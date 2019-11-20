25547ff079b5771462d34aed548c9f2aa80ed308
neko3cs <minaton.0722.net@gmail.com>
2019-11-20 23:28:59 +0900

    init commit

---
+++ ViewModels/ModalWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using PropertyChanged;
+using Reactive.Bindings;
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace XamNumericEditorFocusSample.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class ModalWindowViewModel
+    {
+        public ReactiveProperty<int?> NumberInput { get; } = new ReactiveProperty<int?>();
+        public ReactiveProperty<int?> NumberOutput { get; } = new ReactiveProperty<int?>();
+        public ReactiveCommand ShowTextCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand ClearTextCommand { get; }
+        public ReactiveCommand FocusTextBoxCommand { get; } = new ReactiveCommand();
+
+        public ModalWindowViewModel()
+        {
+            ShowTextCommand.Subscribe(_ =>
+            {
+                NumberOutput.Value = NumberInput.Value;
+                NumberInput.Value = null;
+            });
+
+            ClearTextCommand = NumberOutput
+                .Select(number => !(number is null))
+                .ToReactiveCommand();
+            ClearTextCommand.Subscribe(_ =>
+            {
+                NumberOutput.Value = null;
+                NumberInput.Value = null;
+            });
+        }
+    }
+}
