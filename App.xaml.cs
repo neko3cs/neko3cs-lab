d84eb271bfd18d12b0702b32ac853407815299f4
Minato Suzuki <Minato.Suzuki@jp.ricoh.com>
2019-11-20 23:28:59 +0900

    init commit

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace XamNumericEditorFocusSample
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+    }
+}
