37e9318a5f16a10a9324e81e4ef6ff7cba5e28ab
neko3cs <minaton.0722.net@gmail.com>
2020-11-27 23:10:59 +0900

    support japanese

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 
 namespace PlaySystemTextJson
 {
     class Program
     {
+        private static readonly JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            WriteIndented = true
+        };
+
         static void Main(string[] args)
         {
             SerializeToJsonText();
@@ -15,24 +22,30 @@ namespace PlaySystemTextJson
 
         private static void SerializeToJsonText()
         {
-            var person = new
+            var person1 = new
+            {
+                Name = "おぎゃりすと次郎",
+                Age = 34,
+                Job = "あんぱん職人"
+            };
+            var person2 = new
             {
                 Name = "John Maxwel",
                 Age = 24,
                 Job = "Engineer"
             };
 
-            var json = JsonSerializer.Serialize(person);
-            Console.WriteLine(json);
+            Console.WriteLine(JsonSerializer.Serialize(person1, options));
+            Console.WriteLine(JsonSerializer.Serialize(person2, options));
         }
 
         private static void DeserializeFromJsonText()
         {
-            var json = "{ \"Name\": \"MacBook Pro\", \"Price\": 2879, \"Currency\": \"USD\" }";
+            var json1 = "{ \"Name\": \"あんぱん\", \"Price\": 999999999, \"Currency\": \"JPY\" }";
+            var json2 = "{ \"Name\": \"MacBook Pro\", \"Price\": 2879, \"Currency\": \"USD\" }";
 
-            var product = JsonSerializer.Deserialize<Product>(json);
-
-            Console.WriteLine(product.ToString());
+            Console.WriteLine(JsonSerializer.Deserialize<Product>(json1).ToString());
+            Console.WriteLine(JsonSerializer.Deserialize<Product>(json2).ToString());
         }
     }
 }
