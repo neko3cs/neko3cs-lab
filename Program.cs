f7d78082ca36eba944e66b48c89df99a1bb7bf66
neko3cs <minaton.0722.net@gmail.com>
2020-11-28 17:58:44 +0900

    無駄なコードが多いので削除

--- Program.cs
+++ Program.cs
@@ -6,46 +6,27 @@ namespace PlaySystemTextJson
 {
     class Program
     {
-        private static readonly JsonSerializerOptions options = new JsonSerializerOptions
-        {
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-            WriteIndented = true
-        };
-
         static void Main(string[] args)
         {
-            SerializeToJsonText();
-            Console.WriteLine();
-
-            DeserializeFromJsonText();
-        }
-
-        private static void SerializeToJsonText()
-        {
-            var person1 = new
+            var options = new JsonSerializerOptions
             {
-                Name = "おぎゃりすと次郎",
-                Age = 34,
-                Job = "あんぱん職人"
-            };
-            var person2 = new
-            {
-                Name = "John Maxwel",
-                Age = 24,
-                Job = "Engineer"
+                // 日本語が含まれる場合は "JavaScriptEncoder.UnsafeRelaxedJsonEscaping" を指定する
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                // インデント・改行が入る
+                WriteIndented = true
             };
 
-            Console.WriteLine(JsonSerializer.Serialize(person1, options));
-            Console.WriteLine(JsonSerializer.Serialize(person2, options));
-        }
-
-        private static void DeserializeFromJsonText()
-        {
             var json1 = "{ \"Name\": \"あんぱん\", \"Price\": 999999999, \"Currency\": \"JPY\" }";
             var json2 = "{ \"Name\": \"MacBook Pro\", \"Price\": 2879, \"Currency\": \"USD\" }";
 
-            Console.WriteLine(JsonSerializer.Deserialize<Product>(json1).ToString());
-            Console.WriteLine(JsonSerializer.Deserialize<Product>(json2).ToString());
+            var product1 = JsonSerializer.Deserialize<Product>(json1);
+            var product2 = JsonSerializer.Deserialize<Product>(json2);
+
+            Console.WriteLine(product1.ToString());
+            Console.WriteLine(product2.ToString());
+
+            Console.WriteLine(JsonSerializer.Serialize(product1, options));
+            Console.WriteLine(JsonSerializer.Serialize(product2, options));
         }
     }
 }
