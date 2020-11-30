6f58043060d0dd3921c75c149ac37053d43a4737
neko3cs <minaton.0722.net@gmail.com>
2020-11-30 21:35:27 +0900

    init

---
+++ src/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleAppFramework;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AnnotationInConsoleAppFramework
+{
+    class Program : ConsoleAppBase
+    {
+        static async Task Main(string[] args) =>
+            await Host
+            .CreateDefaultBuilder()
+            .RunConsoleAppFrameworkAsync<Program>(args);
+
+        public void Run(
+            [PathExistsValidation]
+            [Option("path")] string filePath
+        )
+        {
+            Console.WriteLine($"Path here!: {Path.GetFullPath(filePath)}");
+        }
+    }
+}
