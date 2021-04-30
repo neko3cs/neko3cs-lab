5cc6071bf399c91dec3de807be7d81c5b8dcba64
neko3cs <minaton.0722.net@gmail.com>
2021-05-01 05:24:51 +0900

    Windows ServiceでTimerを使ったBackgroundServiceの実装

--- src/SampleRunAppInWindowsDockerContainer.Service/Worker.cs
+++ src/SampleRunAppInWindowsDockerContainer.Service/Worker.cs
@@ -1,26 +1,59 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace SampleRunAppInWindowsDockerContainer.Service
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private Timer _timer;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            _timer = new Timer()
             {
-                _logger.LogInformation("Worker greeting: Hello World");
-                await Task.Delay(1000, stoppingToken);
-            }
+                Interval = TimeSpan.FromSeconds(1).TotalSeconds,
+                Enabled = true,
+                AutoReset = true
+            };
+            _timer.Elapsed += new ElapsedEventHandler(OnElapsed);
+
+            _timer.Start();
+            _logger.LogInformation("Worker greeting: Start Worker");
+
+            return Task.CompletedTask;
+        }
+        private async void OnElapsed(object sender, ElapsedEventArgs e)
+        {
+            _logger.LogInformation("Worker greeting: Hello World");
+
+            await Task.Delay(TimeSpan.FromSeconds(3));
+
+            _logger.LogInformation("Worker greeting: it waited for 3 seconds.");
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer.Stop();
+            _logger.LogInformation("Worker greeting: Stop Worker");
+
+            return base.StopAsync(cancellationToken);
+        }
+
+        public override void Dispose()
+        {
+            _timer.Close();
+            base.Dispose();
         }
     }
 }
