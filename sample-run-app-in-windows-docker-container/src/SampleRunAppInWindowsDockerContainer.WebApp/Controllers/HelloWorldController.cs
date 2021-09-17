3be3357f3fcbda7df8c6224628a057b00752ed68
neko3cs <minaton.0722.net@gmail.com>
2021-09-17 14:44:08 +0900

    Revert "windows docker containerなんてもうやらない"
    This reverts commit cf579642afeaa203a02f3bb1b2ac5da68d79f304.

---
+++ sample-run-app-in-windows-docker-container/src/SampleRunAppInWindowsDockerContainer.WebApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleRunAppInWindowsDockerContainer.WebApp.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() => "Hello World";
+
+        [HttpGet("{name}")]
+        public string Get(string name) => $"Hello {name}";
+    }
+}
---
+++ sample-run-app-in-windows-docker-container/src/SampleRunAppInWindowsDockerContainer.WebApp/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace SampleRunAppInWindowsDockerContainer.WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            })
+            .Build()
+            .Run();
+    }
+}
