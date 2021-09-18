fd53027b2b3553a13033b78e9dfe31bae2edee6f
neko3cs <minaton.0722.net@gmail.com>
2021-09-18 23:40:07 +0900

    ひとまず最後まで実装（TodoInputで_LastNativeTextが取れない問題がある）

--- SampleReactNativeWithQiita/App.js
+++ SampleReactNativeWithQiita/App.js
@@ -1,51 +1,17 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
+ * @flow
  */
 
-import React from 'react';
-import type { Node } from 'react';
+import React, { Component } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  useColorScheme,
   View,
+  FlatList,
 } from 'react-native';
 import TodoInput from './components/TodoInput';
-
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-  _onPress = (text) => {
-    console.log(text);
-  }
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View style={styles.container}>
-          <View style={styles.main}>
-            <TodoInput onPress={this._onPress} />
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+import TodoItem from './components/TodoItem';
 
 const styles = StyleSheet.create({
   container: {
@@ -58,7 +24,82 @@ const styles = StyleSheet.create({
     flex: 1,
     maxWidth: 400,
     alignItems: 'center',
+  },
+  todoListContainer: {
+    flexDirection: 'row',
+    flex: 1,
+  },
+  todoList: {
+    paddingLeft: 10,
+    paddingRight: 10,
   }
 });
 
-export default App;
+export default class App extends Component<{}> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [],
+    };
+  }
+
+  _delete = (index) => () => {
+    const list = [].concat(this.state.list);
+    list.splice(index, 1);
+
+    this.setState({
+      list,
+    });
+  }
+
+  _done = (index) => () => {
+    const list = [].concat(this.state.list);
+    list[index].done = !list[index].done;
+
+    this.setState({
+      list,
+    });
+  }
+
+  _onPress = (text) => {
+    const list = [].concat(this.state.list);
+
+    list.push({
+      key: Date.now(),
+      text: text,
+      done: false,
+    });
+
+    this.setState({
+      list,
+    });
+  }
+
+  render() {
+    const {
+      list,
+    } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.main}>
+          <TodoInput onPress={this._onPress} />
+          <View style={styles.todoListContainer}>
+            <FlatList
+              style={styles.todoList}
+              data={list}
+              renderItem={({ item, index }) => (
+                <TodoItem
+                  onDone={this._done(index)}
+                  onDelete={this._delete(index)}
+                  {...item}
+                />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
