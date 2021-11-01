1acd1297e502a0bfd53c0210b99885e32f44daf9
neko3cs <minaton.0722.net@gmail.com>
2021-11-01 14:30:38 +0900

    余計な事したらバグった

--- sample-chrome-extensions/src/background.js
+++ sample-chrome-extensions/src/background.js
@@ -1,6 +1,6 @@
-const availableURL = "file:///C:/src/neko3cs-lab/sample-chrome-extensions/src/index.html";
-
 const showAlert = () => {
+  const availableURL = "file:///C:/src/neko3cs-lab/sample-chrome-extensions/src/index.html";
+  
   if (document.URL.match(availableURL)) {
     alert("この拡張機能はこのサイトで使えます。");
   } else {
