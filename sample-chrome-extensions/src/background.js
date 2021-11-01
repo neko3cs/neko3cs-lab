862bf45faa44ba1163692e60eba432004afce65a
neko3cs <minaton.0722.net@gmail.com>
2021-11-01 14:12:50 +0900

    chrome-tweet-dialogから学習用コードのみ移設

---
+++ sample-chrome-extensions/src/background.js
@@ -0,0 +1,16 @@
+const availableURL = "file:///C:/src/neko3cs-lab/sample-chrome-extensions/src/index.html";
+
+const showAlert = () => {
+  if (document.URL.match(availableURL)) {
+    alert("この拡張機能はこのサイトで使えます。");
+  } else {
+    alert("この拡張機能はこのサイトで使えません。");
+  }
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: showAlert
+  });
+});
