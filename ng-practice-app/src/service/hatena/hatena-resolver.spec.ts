18940c81f338ddf5346c056d909601548f7fb93a
neko3cs <minaton.0722.net@gmail.com>
2021-12-06 21:42:10 +0900

    フォルダをプレフィクスで分類分け

--- ng-practice-app/src/service/hatena/hatena-resolver.spec.ts
+++
@@ -1,7 +0,0 @@
-import { HatenaResolver } from '../../service/hatena/hatena-resolver';
-
-describe('HatenaResolver', () => {
-  it('should create an instance', () => {
-    expect(new HatenaResolver()).toBeTruthy();
-  });
-});
