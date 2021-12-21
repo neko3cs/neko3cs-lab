6f64a01fb81da22d5521a1be258ad1189c89caf1
neko3cs <minaton.0722.net@gmail.com>
2021-12-22 02:04:16 +0900

    【TRY】画像変換コマンドを作ろう - 指定フォルダ内のJPGを全部PNG に変換するように修正

--- practice-complete-introduction-to-the-golang/05-command-line-tools/extra/main.go
+++ practice-complete-introduction-to-the-golang/05-command-line-tools/extra/main.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 )
 
-var filePath = flag.String("filePath", "", "ファイルパス")
+var dirPath = flag.String("dir", "", "JPGイメージのあるフォルダパス")
 
 func convertJPG2PNG(jpgFilePath string) {
 	jpgFile, err := os.Open(jpgFilePath)
@@ -26,10 +26,10 @@ func convertJPG2PNG(jpgFilePath string) {
 		log.Fatal(err)
 	}
 
-	pngFileName := strings.Replace(filepath.Base(jpgFilePath), ".JPG", ".PNG", -1)
-	fmt.Println("PNG Name: " + pngFileName)
+	pngFilePath := strings.Replace(jpgFilePath, ".JPG", ".PNG", -1)
+	fmt.Println("PNG filepath: " + pngFilePath)
 
-	pngFile, err := os.Create(pngFileName)
+	pngFile, err := os.Create(pngFilePath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -38,7 +38,28 @@ func convertJPG2PNG(jpgFilePath string) {
 	png.Encode(pngFile, image)
 }
 
+func getJPGFilePaths(targetDirPath string) []string {
+	jpgFilePaths := []string{}
+
+	err := filepath.Walk(*dirPath,
+		func(path string, info os.FileInfo, err error) error {
+			if filepath.Ext(path) == ".JPG" {
+				jpgFilePaths = append(jpgFilePaths, path)
+			}
+			return nil
+		})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return jpgFilePaths
+}
+
 func main() {
 	flag.Parse()
-	convertJPG2PNG(*filePath)
+
+	jpgFilePaths := getJPGFilePaths(*dirPath)
+	for _, path := range jpgFilePaths {
+		convertJPG2PNG(path)
+	}
 }
