f290a09419f312c49b70d34b9c547838e601dc5d
neko3cs <minaton.0722.net@gmail.com>
2022-04-18 17:50:32 +0900

    【WIP】プロジェクト作成とテストデータ読み取り処理の実装

---
+++ practice-excel-visual-studio-tools-for-office/PracticeExcelVisualStudioToolsForOffice/MainRibbon.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace PracticeExcelVisualStudioToolsForOffice
+{
+    partial class MainRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public MainRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.RunButton = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.RunButton);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // RunButton
+            // 
+            this.RunButton.Label = "実行";
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.RunButton_Click);
+            // 
+            // MainRibbon
+            // 
+            this.Name = "MainRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.MainRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton RunButton;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal MainRibbon MainRibbon
+        {
+            get { return this.GetRibbon<MainRibbon>(); }
+        }
+    }
+}
---
+++ practice-excel-visual-studio-tools-for-office/PracticeExcelVisualStudioToolsForOffice/MainRibbon.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Ribbon;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PracticeExcelVisualStudioToolsForOffice
+{
+    public partial class MainRibbon
+    {
+        private void MainRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void RunButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            var sheets = Globals.ThisAddIn.Application.Sheets;
+
+            var tables = new List<Dictionary<string, string>>();
+            foreach (Worksheet sheet in sheets)
+            {
+                for (var rowIndex = 0; rowIndex < sheet.UsedRange.Rows.Count; rowIndex++)
+                {
+                    for (var colIndex = 0; colIndex < sheet.UsedRange.Columns.Count; colIndex++)
+                    {
+                        var value = sheet.Range[rowIndex, colIndex].Cells.Value.ToString();
+                    }
+                }
+            }
+
+            //SaveAsFile();
+        }
+
+        private static void SaveAsFile(List<Dictionary<string, string>> tables)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.FileName = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (var stream = saveFileDialog.OpenFile())
+                {
+                    if (stream is null) throw new FileNotFoundException("ファイルが見つからないか、開けませんでした。");
+                }
+            }
+        }
+    }
+}
