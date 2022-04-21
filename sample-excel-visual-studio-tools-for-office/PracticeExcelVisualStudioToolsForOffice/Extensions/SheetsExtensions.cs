e14d2b0541b787548bc76e617b4c516225523512
neko3cs <minaton.0722.net@gmail.com>
2022-04-21 18:10:20 +0900

    リファクタリング

---
+++ sample-excel-visual-studio-tools-for-office/PracticeExcelVisualStudioToolsForOffice/Extensions/SheetsExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Office.Interop.Excel;
+using PracticeExcelVisualStudioToolsForOffice.Models;
+using System.Collections.Generic;
+
+namespace PracticeExcelVisualStudioToolsForOffice.Extensions
+{
+    public static class SheetsExtensions
+    {
+        public static IEnumerable<Table> ConvertToTables(this Sheets sheets)
+        {
+            foreach (Worksheet sheet in sheets)
+            {
+                var table = new Table(sheet.Name);
+                for (int rowIndex = 3; rowIndex < sheet.UsedRange.Rows.Count; rowIndex++) // データは3行目から入ってる
+                {
+                    var row = new Row();
+                    for (int columnIndex = 1; columnIndex <= sheet.UsedRange.Columns.Count; columnIndex++)
+                    {
+                        row.AddColumn(
+                            columnName: sheet.Cells[1, columnIndex].Value.ToString(),
+                            value: sheet.Cells[rowIndex, columnIndex].Value.ToString()
+                        );
+                    }
+                    table.AddRow(row);
+                }
+                yield return table;
+            }
+        }
+    }
+}
