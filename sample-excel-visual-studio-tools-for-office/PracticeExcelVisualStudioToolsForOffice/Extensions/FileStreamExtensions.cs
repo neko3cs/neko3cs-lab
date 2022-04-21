d8cf76da5c688cb62d68351de45b6dcd52a8da8e
neko3cs <minaton.0722.net@gmail.com>
2022-04-21 17:25:28 +0900

    データ取得処理の実装：完成

---
+++ sample-excel-visual-studio-tools-for-office/PracticeExcelVisualStudioToolsForOffice/Extensions/FileStreamExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace PracticeExcelVisualStudioToolsForOffice.Extensions
+{
+    public static class FileStreamExtensions
+    {
+        public static void WriteText(this FileStream stream, string value)
+        {
+            var info = new UTF8Encoding(true).GetBytes(value);
+            stream.Write(info, 0, info.Length);
+        }
+
+        public static void WriteTextLine(this FileStream stream, string value = "") =>
+            WriteText(stream, $"{value}{Environment.NewLine}");
+    }
+}
---
+++ sample-excel-visual-studio-tools-for-office/PracticeExcelVisualStudioToolsForOffice/Models/Table.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PracticeExcelVisualStudioToolsForOffice.Models
+{
+    public class Table
+    {
+        private readonly List<Row> _rows = new List<Row>();
+
+        public string Name { get; }
+        public IReadOnlyList<Row> Rows => _rows;
+
+        public Table(string name) => Name = name;
+
+        public void AddRow(Row row) => _rows.Add(row);
+    }
+}
