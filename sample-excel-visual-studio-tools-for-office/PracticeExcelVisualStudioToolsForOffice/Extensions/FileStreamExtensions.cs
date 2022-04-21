dbc9b7601f848b1f31de8625964d4ff127c0ac0e
neko3cs <minaton.0722.net@gmail.com>
2022-04-21 18:10:20 +0900

    リファクタリング

--- sample-excel-visual-studio-tools-for-office/PracticeExcelVisualStudioToolsForOffice/Extensions/FileStreamExtensions.cs
+++ sample-excel-visual-studio-tools-for-office/PracticeExcelVisualStudioToolsForOffice/Extensions/FileStreamExtensions.cs
@@ -1,18 +1,39 @@
-﻿using System;
+﻿using PracticeExcelVisualStudioToolsForOffice.Models;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace PracticeExcelVisualStudioToolsForOffice.Extensions
 {
     public static class FileStreamExtensions
     {
-        public static void WriteText(this FileStream stream, string value)
+        public static void SaveAsCsv(this FileStream stream, IEnumerable<Table> tables)
+        {
+            foreach (Table table in tables)
+            {
+                stream.WriteTextLine($"◆{table.Name}");
+
+                var csvHeader = string.Join(",", table.Rows.First().Columns.Keys);
+                stream.WriteTextLine(csvHeader);
+
+                foreach (Row row in table.Rows)
+                {
+                    var csvRecord = string.Join(",", row.Columns.Values);
+                    stream.WriteTextLine(csvRecord);
+                }
+
+                stream.WriteTextLine();
+            }
+        }
+        private static void WriteText(this FileStream stream, string value)
         {
             var info = new UTF8Encoding(true).GetBytes(value);
             stream.Write(info, 0, info.Length);
         }
 
-        public static void WriteTextLine(this FileStream stream, string value = "") =>
+        private static void WriteTextLine(this FileStream stream, string value = "") =>
             WriteText(stream, $"{value}{Environment.NewLine}");
     }
 }
--- sample-excel-visual-studio-tools-for-office/PracticeExcelVisualStudioToolsForOffice/MainRibbon.cs
+++ sample-excel-visual-studio-tools-for-office/PracticeExcelVisualStudioToolsForOffice/MainRibbon.cs
@@ -1,82 +1,37 @@
-﻿using Microsoft.Office.Interop.Excel;
-using Microsoft.Office.Tools.Ribbon;
+﻿using Microsoft.Office.Tools.Ribbon;
 using PracticeExcelVisualStudioToolsForOffice.Extensions;
-using PracticeExcelVisualStudioToolsForOffice.Models;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Windows.Forms;
+using Application = Microsoft.Office.Interop.Excel.Application;
 
 namespace PracticeExcelVisualStudioToolsForOffice
 {
     public partial class MainRibbon
     {
+        private Application Excel => Globals.ThisAddIn.Application;
+
         private void MainRibbon_Load(object sender, RibbonUIEventArgs e) { }
 
         private void RunButton_Click(object sender, RibbonControlEventArgs e)
         {
-            using (var stream = GetStreamFromFileDialog())
+            var saveFileDialog = new SaveFileDialog
             {
-                var tables = ConvertToTables(Globals.ThisAddIn.Application.Sheets);
-                Save(stream, tables);
+                Filter = @"csv files (*.csv)|*.csv",
+                RestoreDirectory = true
+            };
+            if (saveFileDialog.ShowDialog() != DialogResult.OK) return;
 
-                MessageBox.Show(@"テストデータ作成ツール", @"出力が完了しました。", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-        }
-
-        private FileStream GetStreamFromFileDialog()
-        {
-            var saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = @"txt files (*.txt)|*.txt|All files (*.*)|*.*";
-            saveFileDialog.FilterIndex = 2;
-            saveFileDialog.RestoreDirectory = true;
-
-            if (saveFileDialog.ShowDialog() != DialogResult.OK) return null;
-
-            var stream = saveFileDialog.OpenFile();
-            if (stream is null) throw new FileNotFoundException();
-
-            return stream as FileStream;
-        }
-
-        private IEnumerable<Table> ConvertToTables(Sheets sheets)
-        {
-            foreach (Worksheet sheet in sheets)
+            using (var stream = saveFileDialog.OpenFile() as FileStream)
             {
-                var table = new Table(sheet.Name);
-                for (int rowIndex = 3; rowIndex < sheet.UsedRange.Rows.Count; rowIndex++) // データは3行目から入ってる
-                {
-                    var row = new Row();
-                    for (int columnIndex = 1; columnIndex <= sheet.UsedRange.Columns.Count; columnIndex++)
-                    {
-                        row.AddColumn(
-                            columnName: sheet.Cells[1, columnIndex].Value.ToString(),
-                            value: sheet.Cells[rowIndex, columnIndex].Value.ToString()
-                        );
-                    }
-                    table.AddRow(row);
-                }
-                yield return table;
-            }
-        }
+                var tables = Excel.Sheets.ConvertToTables();
+                stream.SaveAsCsv(tables);
 
-        private void Save(FileStream stream, IEnumerable<Table> tables)
-        {
-            foreach (Table table in tables)
-            {
-                stream.WriteTextLine($"◆{table.Name}");
-
-                var csvHeader = string.Join(",", table.Rows.First().Columns.Keys);
-                stream.WriteTextLine(csvHeader);
-
-                foreach (Row row in table.Rows)
-                {
-                    var csvRecord = string.Join(",", row.Columns.Values);
-                    stream.WriteTextLine(csvRecord);
-                }
-
-                stream.WriteTextLine();
+                MessageBox.Show(
+                    text: @"テストデータ作成ツール",
+                    caption: @"出力が完了しました。",
+                    buttons: MessageBoxButtons.OK,
+                    icon: MessageBoxIcon.Information
+                );
             }
         }
     }
