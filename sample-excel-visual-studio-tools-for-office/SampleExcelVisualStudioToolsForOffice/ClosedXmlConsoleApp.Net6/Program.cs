e15824e88a368df16370cf6ba829ec1bf3cb261d
neko3cs <minaton.0722.net@gmail.com>
2022-04-24 02:01:23 +0900

    比較用のツールとしてClosedXMLを用いたものを用意した

---
+++ sample-excel-visual-studio-tools-for-office/SampleExcelVisualStudioToolsForOffice/ClosedXmlConsoleApp.Net6/Program.cs
@@ -0,0 +1,9 @@
+﻿using ClosedXML.Excel;
+using ClosedXmlConsoleApp.Net6;
+
+ConsoleApp.Run(args, (string input, string output) =>
+{
+    var workbook = new XLWorkbook(input);
+    var tables = workbook.Load();
+    tables.ExportTo(output);
+});
---
+++ sample-excel-visual-studio-tools-for-office/SampleExcelVisualStudioToolsForOffice/ClosedXmlConsoleApp.Net6/Table.cs
@@ -0,0 +1,14 @@
+﻿namespace ClosedXmlConsoleApp.Net6
+{
+    public class Table
+    {
+        private readonly List<Row> _rows = new List<Row>();
+
+        public string Name { get; }
+        public IReadOnlyList<Row> Rows => _rows;
+
+        public Table(string name) => Name = name;
+
+        public void AddRow(Row row) => _rows.Add(row);
+    }
+}
