e4e37a280e4de5849b3c97234281085a6e889bcd
neko3cs <minaton.0722.net@gmail.com>
2022-04-25 16:24:45 +0900

    時間計測の処理を追加

--- sample-excel-visual-studio-tools-for-office/SampleExcelVisualStudioToolsForOffice/ClosedXmlConsoleApp.Net6/Program.cs
+++ sample-excel-visual-studio-tools-for-office/SampleExcelVisualStudioToolsForOffice/ClosedXmlConsoleApp.Net6/Program.cs
@@ -1,9 +1,16 @@
 ﻿using ClosedXML.Excel;
 using ClosedXmlConsoleApp.Net6;
+using System.Diagnostics;
 
 ConsoleApp.Run(args, (string input, string output) =>
 {
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+
     var workbook = new XLWorkbook(input);
     var tables = workbook.Load();
     tables.ExportTo(output);
+
+    stopwatch.Stop();
+    File.AppendAllText(output, $@"★処理時間(ms)：{stopwatch.Elapsed.TotalMilliseconds:g}{Environment.NewLine}");
 });
