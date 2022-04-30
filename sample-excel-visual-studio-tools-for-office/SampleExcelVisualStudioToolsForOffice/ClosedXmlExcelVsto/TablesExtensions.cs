0e37b2e0fad9197c8f5603bd838cdbe3d05ac9c8
neko3cs <minaton.0722.net@gmail.com>
2022-04-30 18:55:05 +0900

    Excel VSTO+ClosedXml版の作成（動作未確認）

---
+++ sample-excel-visual-studio-tools-for-office/SampleExcelVisualStudioToolsForOffice/ClosedXmlExcelVsto/TablesExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ClosedXmlExcelVsto
+{
+    public static class TablesExtensions
+    {
+        public static void ExportTo(this IEnumerable<Table> tables, string filePath)
+        {
+            if (File.Exists(filePath)) File.Delete(filePath);
+
+            using (var writer = File.CreateText(filePath))
+            {
+                foreach (var table in tables)
+                {
+                    writer.WriteLine($"◆{table.Name}");
+
+                    var csvHeader = string.Join(",", table.Rows.First().Columns.Keys);
+                    writer.WriteLine(csvHeader);
+
+                    foreach (var row in table.Rows)
+                    {
+                        var csvRecord = string.Join(",", row.Columns.Values);
+                        writer.WriteLine(csvRecord);
+                    }
+
+                    writer.WriteLine();
+                }
+            }
+        }
+    }
+}
