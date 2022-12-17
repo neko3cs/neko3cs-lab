7417b0eca48ca38451dd5635626ce635f06265d7
neko3cs <minaton.0722.net@gmail.com>
2022-12-17 23:32:09 +0900

    スイーツオブジェクト

---
+++ sample-basic-asp-net-mvc-app/src/SampleBasicAspNetMvcApp.Domain.Test/SweetsTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace SampleBasicAspNetMvcApp.Domain.Test;
+
+public class SweetsTest
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ sample-basic-asp-net-mvc-app/src/SampleBasicAspNetMvcApp.Domain/Sweets.cs
@@ -0,0 +1,20 @@
+﻿namespace SampleBasicAspNetMvcApp.Domain;
+
+public class Sweets : ISweets
+{
+
+    public Sweets(
+        int id,
+        string name,
+        int calories
+    )
+    {
+        Id = id;
+        Name = name;
+        Calories = calories;
+    }
+
+    public int Id { get; }
+    public string Name { get; }
+    public int Calories { get; }
+}
