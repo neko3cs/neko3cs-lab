67102ca3b0f1f7d56e878faf52ae53faf5905ec6
neko3cs <minaton.0722.net@gmail.com>
2022-12-17 21:32:28 +0900

    delete attach directory

--- sample-basic-asp-net-mvc-app/db/startup_database.ps1
+++ sample-basic-asp-net-mvc-app/db/startup_database.ps1
@@ -7,5 +7,4 @@ docker run `
     -e 'SA_PASSWORD=P@ssword!' `
     -p 1434:1433 `
     --name sql1 `
-    -v /src/neko3cs-lab/sample-basic-asp-net-mvc-app/db:/tmp `
     -d mcr.microsoft.com/mssql/server:latest
