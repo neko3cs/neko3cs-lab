3999763af589a33e5ed55d29ce5ab58f6c411801
neko3cs <minaton.0722.net@gmail.com>
2022-12-17 23:33:20 +0900

    [WIP]add sweetsrepository

--- sample-basic-asp-net-mvc-app/src/SampleBasicAspNetMvcApp.Infrastructure.Test/SweetsRepositoryTest.cs
+++ sample-basic-asp-net-mvc-app/src/SampleBasicAspNetMvcApp.Infrastructure.Test/SweetsRepositoryTest.cs
@@ -1,9 +1,13 @@
-namespace SampleBasicAspNetMvcApp.Infrastructure.Test
+using Xunit;
+
+namespace SampleBasicAspNetMvcApp.Infrastructure.Test;
+
+public class SweetsRepositoryTest
 {
-    public class SweetsRepositoryTest
+    public class スイーツの一覧を取得する
     {
         [Fact]
-        public void Test1()
+        public void 一覧が取得出来ること()
         {
 
         }
