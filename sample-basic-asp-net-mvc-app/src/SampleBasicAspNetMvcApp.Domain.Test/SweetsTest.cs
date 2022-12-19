2810287b8c2fa8716864048fd2590db0459efccb
neko3cs <minaton.0722.net@gmail.com>
2022-12-19 16:44:21 +0900

    スイーツモデルのテスト

--- sample-basic-asp-net-mvc-app/src/SampleBasicAspNetMvcApp.Domain.Test/SweetsTest.cs
+++ sample-basic-asp-net-mvc-app/src/SampleBasicAspNetMvcApp.Domain.Test/SweetsTest.cs
@@ -4,9 +4,16 @@ namespace SampleBasicAspNetMvcApp.Domain.Test;
 
 public class SweetsTest
 {
-    [Fact]
-    public void Test1()
+    public class 名前を取得する
     {
+        [Fact]
+        public void 名前が取得出来ること()
+        {
+            const string name = "チーズケーキ";
 
+            var sweets = new Sweets(1, name, 0);
+
+            Assert.Equal(name, sweets.Name);
+        }
     }
 }
