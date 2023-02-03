30793c7be46849234645bd3b454bba8979ca0d82
neko3cs <minaton.0722.net@gmail.com>
2023-02-04 01:28:31 +0900

    ✨ToDoアプリの実装

--- practice-tauri-todo-app/todo-app/src/App.tsx
+++ practice-tauri-todo-app/todo-app/src/App.tsx
@@ -1,48 +1,32 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+import { open } from "@tauri-apps/api/dialog";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [tickets, setTickets] = useState([""]);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  async function openDialog() {
+    /**1. @tauri-apps/api/dialog#openで選択したファイルのパスを取得。 */
+    const path = await open({ multiple: false });
+    if (typeof path === "string") {
+      /**2. create_ticketコマンドを実行。String[]で受け取ったチケット一覧をセット。 */
+      setTickets(await invoke<string[]>("create_ticket", { path: path }));
+    }
   }
+
+  const ticketsEl = tickets.map((ticket) => <p>{ticket}</p>);
 
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
 
       <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={openDialog}>add tickets from file</button>
+        <button onClick={() => setTickets([])}>clear tickets</button>
       </div>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
-        </div>
-      </div>
-      <p>{greetMsg}</p>
+      {ticketsEl}
     </div>
   );
 }
