2667927c4e168e6374de802ad0b3899abd9172cd
neko3cs <minaton.0722.net@gmail.com>
2023-03-30 16:53:47 +0900

    refactor: 既存コードが汚いので修正

--- sample/basic-dotnet-maui-comet/src/MainPage.cs
+++ sample/basic-dotnet-maui-comet/src/MainPage.cs
@@ -1,42 +1,36 @@
 namespace BasicDotnetMauiComet;
+
 public class MainPage : View
 {
 
-	[State]
-	readonly CometRide comet = new();
+    [State]
+    private readonly CometRide _comet = new();
 
-	[Body]
-	View body()
-		=> new VStack {
-				new Text(()=> $"({comet.Rides}) rides taken:{comet.CometTrain}")
-					.Frame(width:300)
-					.LineBreakMode(LineBreakMode.CharacterWrap),
+    [Body]
+    private new View Body() => new VStack {
+        new Text(()=> $"({_comet.Rides}) rides taken:{_comet.CometTrain}")
+            .Frame(width:300)
+            .LineBreakMode(LineBreakMode.CharacterWrap),
 
-				new Button("Ride the Comet! ☄️", ()=>{
-					comet.Rides++;
-				})
-					.Frame(height:44)
-					.Margin(8)
-					.Color(Colors.White)
-					.Background(Colors.Green)
-				.RoundedBorder(color:Colors.Blue)
-				.Shadow(Colors.Grey,4,2,2),
-		};
+        new Button("Ride the Comet! ☄️", ()=>{
+            _comet.Rides++;
+        })
+            .Frame(height:44)
+            .Margin(8)
+            .Color(Colors.White)
+            .Background(Colors.Green)
+        .RoundedBorder(color:Colors.Blue)
+        .Shadow(Colors.Grey,4,2,2),
+    };
 
-	public class CometRide : BindingObject
-	{
-		public int Rides
-		{
-			get => GetProperty<int>();
-			set => SetProperty(value);
-		}
+    public class CometRide : BindingObject
+    {
+        public int Rides
+        {
+            get => GetProperty<int>();
+            set => SetProperty(value);
+        }
 
-		public string CometTrain
-		{
-			get
-			{
-				return "☄️".Repeat(Rides);
-			}
-		}
-	}
+        public string CometTrain => "☄️".Repeat(Rides);
+    }
 }
