f12c73a4876933cee0c549c0b167d0c99797b014
neko3cs <minaton.0722.net@gmail.com>
2023-03-30 16:53:47 +0900

    refactor: 既存コードが汚いので修正

--- sample/basic-dotnet-maui-comet/src/App.cs
+++ sample/basic-dotnet-maui-comet/src/App.cs
@@ -1,24 +1,24 @@
 using Microsoft.Maui.Hosting;
 
 namespace BasicDotnetMauiComet;
+
 public class App : CometApp
 {
-	[Body]
-	View view() => new MainPage();
+    [Body]
+    private View View() => new MainPage();
 
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder.UseCometApp<App>()
-			.ConfigureFonts(fonts => {
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
-		//-:cnd
+    public static MauiApp CreateMauiApp() => MauiApp
+        .CreateBuilder()
+        .UseCometApp<App>()
+        .ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        })
+        //-:cnd
 #if DEBUG
-		builder.EnableHotReload();
+        .EnableHotReload()
 #endif
-		//+:cnd
-		return builder.Build();
-	}
+        //+:cnd
+        .Build();
 }
