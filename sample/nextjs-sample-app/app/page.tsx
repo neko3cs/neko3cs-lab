7689e7ab7748f2cd5bd9d4512a4b4344a3c2bb99
neko3cs <minaton.0722.net@gmail.com>
2023-09-14 13:32:23 +0900

    feature: 「JSXでCSSの書き方」まで実施

--- sample/nextjs-sample-app/app/page.tsx
+++ sample/nextjs-sample-app/app/page.tsx
@@ -1,8 +1,27 @@
+import Head from 'next/head'
+import Styles from './Home.module.css'
+
 export default function Home() {
+  let title = "ともすた";
+
   return (
     <>
-      <h1>ともすた</h1>
+      {/* 最新バージョンだとlayout.tsxのMetadataに記載するのが一般的そう
+      <Head>
+        <title>ともすた</title>
+      </Head>
+      */}
+
+      <h1 className={Styles.mytitle} style={{ backgroundColor: `#9f9` }}>{title}</h1>
       <p>学ぶ。をちゃんと</p>
+
+      {/*
+      <style jsx>{`
+        h1 {
+          color: white;
+        }
+      `}</style>
+      */}
     </>
   )
 }
