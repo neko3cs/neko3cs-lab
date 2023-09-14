5b48f4859166f46086a2fc6e02893381fa2a794a
neko3cs <minaton.0722.net@gmail.com>
2023-09-14 21:45:18 +0900

    feature: 「SWRでJSONデータを取得しよう」まで実施

---
+++ sample/nextjs-sample-app/app/message/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest
+) {
+  return NextResponse.json(
+    { message: "学ぶ。をちゃんと" },
+    { status: 200 }
+  );
+}
