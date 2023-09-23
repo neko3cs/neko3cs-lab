359861da1d35dbcce1fcb13f2141e0b12d98994a
neko3cs <minaton.0722.net@gmail.com>
2023-09-23 16:46:28 +0900

    [WIP]feature: モデルの作成

---
+++ sample/send-mail-with-dotnet/SendMailWithDotNet/Models/SendMail.cs
@@ -0,0 +1,54 @@
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using SendMailWithDotNet.Models;
+
+namespace SendMailWithDotNet;
+
+public class SendMail
+{
+  private readonly string SmtpHost = "smtp.gmail.com";
+  private readonly int SmtpPort = 465;
+  private readonly Account _account;
+
+  public SendMail(Account account)
+  {
+    _account = account;
+  }
+
+  public async Task SendAsync(
+    string subject,
+    string body,
+    List<MailboxAddress> toList,
+    List<MailboxAddress> ccList = null,
+    List<MailboxAddress> bccList = null
+  )
+  {
+    var message = new MimeMessage();
+    message.From.Add(new MailboxAddress(_account.Name, _account.Address));
+
+    toList.ForEach(to => message.To.Add(to));
+    ccList?.ForEach(cc => message.Cc.Add(cc));
+    bccList?.ForEach(bcc => message.Bcc.Add(bcc));
+
+    message.Subject = subject;
+    message.Body = new TextPart("plain") { Text = body };
+
+    try
+    {
+      using var client = new SmtpClient();
+      // サーバ接続
+      await client.ConnectAsync(SmtpHost, SmtpPort, SecureSocketOptions.SslOnConnect);
+      // SMTP認証
+      await client.AuthenticateAsync(_account.Address, _account.Password);
+      // メール送信
+      await client.SendAsync(message);
+      // サーバ接続解除
+      await client.DisconnectAsync(true);
+    }
+    catch (Exception ex)
+    {
+      throw;
+    }
+  }
+}
