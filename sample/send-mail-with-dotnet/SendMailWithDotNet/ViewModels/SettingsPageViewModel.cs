e3d0b4352618921aad2406b1a62b6740dbad4b27
neko3cs <minaton.0722.net@gmail.com>
2023-09-25 15:52:15 +0900

    feature: ビルド通るように修正（.NET 8.0を待つ）
    どうやらCommunity ToolKitが8.0にならないとまともに使えなさそうなのでそれを待つ

--- sample/send-mail-with-dotnet/SendMailWithDotNet/ViewModels/SettingsPageViewModel.cs
+++ sample/send-mail-with-dotnet/SendMailWithDotNet/ViewModels/SettingsPageViewModel.cs
@@ -1,40 +1,40 @@
-using SendMailWithDotNet;
-using SendMailWithDotNet.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using SendMailWithDotNet.Models;
+
+namespace SendMailWithDotNet.ViewModels;
 
 public partial class SettingsPageViewModel : ObservableObject
 {
-  [ObservableProperty]
-  [NotifyPropertyChangedFor(nameof(Name))]
-  private string _name;
-  public string Name => _name;
-  [ObservableProperty]
-  [NotifyPropertyChangedFor(nameof(Address))]
-  private string _address;
-  public string Address => _address;
-  [ObservableProperty]
-  [NotifyPropertyChangedFor(nameof(Password))]
-  private string _password;
-  public string Password => _password;
+    [field: ObservableProperty]
+    [field: NotifyPropertyChangedFor(nameof(Name))]
+    public string Name { get; private set; }
 
-  [RelayCommand]
-  public void SaveUserInfoCommand()
-  {
-    if (
-      string.IsNullOrEmpty(Name) ||
-      string.IsNullOrEmpty(Address) ||
-      string.IsNullOrEmpty(Password)
-    )
+    [field: ObservableProperty]
+    [field: NotifyPropertyChangedFor(nameof(Address))]
+    public string Address { get; private set; }
+
+    [field: ObservableProperty]
+    [field: NotifyPropertyChangedFor(nameof(Password))]
+    public string Password { get; private set; }
+
+    [RelayCommand]
+    public void SaveUserInfoCommand()
     {
-      // await PageDialogService.DisplayAlertAsync("There is blank area.", "Please check input area.", "OK");
-      return;
-    }
+        if (
+            string.IsNullOrEmpty(Name) ||
+            string.IsNullOrEmpty(Address) ||
+            string.IsNullOrEmpty(Password)
+        )
+        {
+            // await PageDialogService.DisplayAlertAsync("There is blank area.", "Please check input area.", "OK");
+            return;
+        }
 
-    App.Account = new Account(
-      Name,
-      Address,
-      Password
-    );
-  }
+        App.Account = new Account(
+            Name,
+            Address,
+            Password
+        );
+    }
 }
