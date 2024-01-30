277fe6d85723728ffd025c65ffc7f14a29e71f58
neko3cs <minaton.0722.net@gmail.com>
2024-01-30 18:21:46 +0900

    feature: 1.3 ボタンを作ってみよう(QPushButton)

--- practice/introduce-to-qt-prog/main.cpp
+++ practice/introduce-to-qt-prog/main.cpp
@@ -1,10 +1,12 @@
 #include <QtWidgets/QApplication>
-#include <QLabel>
+#include <QPushButton>
 
 int main(int argc, char **argv)
 {
   QApplication app(argc, argv);
-  QLabel *label = new QLabel("<h2><i>Hello<br>Qt!</i></h2>");
-  label->show();
+  QPushButton *button = new QPushButton("Hello Qt!");
+  button->resize(200, 50);
+  button->move(100, 50);
+  button->show();
   return app.exec();
 }
