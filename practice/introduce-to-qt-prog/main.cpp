aa7896ba305df7c039873f1f0eaa42247ac732de
neko3cs <minaton.0722.net@gmail.com>
2024-01-30 18:42:20 +0900

    feature: 3.1 SIGNAL/SLOTの基本的な使い方

--- practice/introduce-to-qt-prog/main.cpp
+++ practice/introduce-to-qt-prog/main.cpp
@@ -1,30 +1,12 @@
 #include <QApplication>
 #include <QPushButton>
-#include <QHBoxLayout>
-#include <QVBoxLayout>
 
 int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
-	QWidget* window = new QWidget;
-	QPushButton* buttonA = new QPushButton("Button A");
-	QPushButton* buttonB = new QPushButton("Button B");
-	QPushButton* buttonC = new QPushButton("Button C");
-	QPushButton* buttonD = new QPushButton("Button D");
-	
-	QVBoxLayout* mainLayout = new QVBoxLayout;
-	QHBoxLayout* layoutA = new QHBoxLayout;
-	QVBoxLayout* layoutB = new QVBoxLayout;
-
-	layoutA->addWidget(buttonA);
-	layoutA->addWidget(buttonB);
-	layoutB->addWidget(buttonC);
-	layoutB->addWidget(buttonD);
-
-	mainLayout->addLayout(layoutA);
-	mainLayout->addLayout(layoutB);
-
-	window->setLayout(mainLayout);
-	window->show();	
+	QPushButton* button = new QPushButton("Quit");
+	QObject::connect(button, SIGNAL( clicked() ),
+			&app, SLOT(quit()) );
+	button->show();
 	return app.exec();
 }
