5ec975ea3f68cffe5e641ac76bb2e4d2de6759a6
neko3cs <minaton.0722.net@gmail.com>
2024-01-30 18:44:51 +0900

    feature: 3.2 複数のSlotについて

--- practice/introduce-to-qt-prog/main.cpp
+++ practice/introduce-to-qt-prog/main.cpp
@@ -1,12 +1,25 @@
 #include <QApplication>
-#include <QPushButton>
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QLineEdit>
 
 int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
-	QPushButton* button = new QPushButton("Quit");
-	QObject::connect(button, SIGNAL( clicked() ),
-			&app, SLOT(quit()) );
-	button->show();
+	QLabel* label = new QLabel("Hello");
+	QLineEdit* edit = new QLineEdit;
+	QWidget* window = new QWidget;
+	QHBoxLayout* layout = new QHBoxLayout;
+	
+	QObject::connect(edit,SIGNAL(textChanged(QString)),
+			label,SLOT(setText(QString)) );
+	QObject::connect(edit,SIGNAL(textChanged(QString)),
+			window,SLOT(setWindowTitle(QString)) );
+	
+	layout->addWidget(edit);
+	layout->addWidget(label);
+	window->setLayout(layout);
+	window->show();
+
 	return app.exec();
 }
