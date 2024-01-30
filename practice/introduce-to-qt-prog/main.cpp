b82a88c1612ff6c726645643e066fc3b1ecee3a5
neko3cs <minaton.0722.net@gmail.com>
2024-01-30 18:46:48 +0900

    feature: 3.3 Connection の解除

--- practice/introduce-to-qt-prog/main.cpp
+++ practice/introduce-to-qt-prog/main.cpp
@@ -1,25 +1,14 @@
 #include <QApplication>
-#include <QLabel>
-#include <QHBoxLayout>
-#include <QLineEdit>
+#include <QPushButton>
 
 int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
-	QLabel* label = new QLabel("Hello");
-	QLineEdit* edit = new QLineEdit;
-	QWidget* window = new QWidget;
-	QHBoxLayout* layout = new QHBoxLayout;
-	
-	QObject::connect(edit,SIGNAL(textChanged(QString)),
-			label,SLOT(setText(QString)) );
-	QObject::connect(edit,SIGNAL(textChanged(QString)),
-			window,SLOT(setWindowTitle(QString)) );
-	
-	layout->addWidget(edit);
-	layout->addWidget(label);
-	window->setLayout(layout);
-	window->show();
-
+	QPushButton* button = new QPushButton("Quit");
+	QObject::connect(button, SIGNAL( clicked() ),
+			&app, SLOT(quit()) );
+	QObject::disconnect(button, SIGNAL( clicked() ),
+			&app, SLOT(quit()) );
+	button->show();
 	return app.exec();
 }
