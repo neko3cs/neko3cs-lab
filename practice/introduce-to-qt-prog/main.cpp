79ecc9ddc83e493305ea8e02176eb9bad09a91ee
neko3cs <minaton.0722.net@gmail.com>
2024-01-30 18:23:46 +0900

    feature: 1.4 フォントを変えてみよう(QFont)

--- practice/introduce-to-qt-prog/main.cpp
+++ practice/introduce-to-qt-prog/main.cpp
@@ -1,5 +1,6 @@
 #include <QtWidgets/QApplication>
 #include <QPushButton>
+#include <QFont>
 
 int main(int argc, char **argv)
 {
@@ -7,6 +8,7 @@ int main(int argc, char **argv)
   QPushButton *button = new QPushButton("Hello Qt!");
   button->resize(200, 50);
   button->move(100, 50);
+  button->setFont(QFont("Times", 15, QFont::Bold, true));
   button->show();
   return app.exec();
 }
