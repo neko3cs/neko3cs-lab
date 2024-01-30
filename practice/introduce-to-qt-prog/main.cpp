c91d852be2b87aa7b050d1b1f4abaf05107f797c
neko3cs <minaton.0722.net@gmail.com>
2024-01-30 18:32:52 +0900

    feature: 2.2 基本的なLayoutの使い方

--- practice/introduce-to-qt-prog/main.cpp
+++ practice/introduce-to-qt-prog/main.cpp
@@ -1,12 +1,21 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QHBoxLayout>
 
 int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
-	QPushButton* button = new QPushButton("Hello Qt!");
-	QPushButton* button2 = new QPushButton("Goodbye");
-	button->show();
-	button2->show();
+	QWidget* window = new QWidget;
+	QPushButton* buttonA = new QPushButton("Button A");
+	QPushButton* buttonB = new QPushButton("Button B");
+	QPushButton* buttonC = new QPushButton("Button C");
+	QHBoxLayout* layout = new QHBoxLayout;
+
+	layout->addWidget(buttonA);
+	layout->addWidget(buttonB);
+	layout->addWidget(buttonC);
+
+	window->setLayout(layout);
+	window->show();	
 	return app.exec();
 }
