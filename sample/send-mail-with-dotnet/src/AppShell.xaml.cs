be7b7cea4731fbffd580083f01e2e765f83fb2dd
neko3cs <minaton.0722.net@gmail.com>
2024-02-16 14:49:42 +0900

    feature: net8.0へのアップデートに伴うプロジェクト作り直し

---
+++ sample/send-mail-with-dotnet/src/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SendMailWithDotnet;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ sample/send-mail-with-dotnet/src/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SendMailWithDotnet;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
