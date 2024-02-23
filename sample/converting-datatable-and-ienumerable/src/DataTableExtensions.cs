d9c12533a7d51aa3130198a91b6cdfe0733bf3bd
neko3cs <minaton.0722.net@gmail.com>
2024-02-23 17:38:24 +0900

    feature: DataTable型とIEnumerable型の変換サンプルコードの作成

---
+++ sample/converting-datatable-and-ienumerable/src/DataTableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ConvertingDataTableAndIEnumerable;
+
+public static class DataTableExtensions
+{
+  public static IEnumerable<T> ToEnumerable<T>(this DataTable table) where T : new()
+  {
+    var props = typeof(T).GetProperties();
+    foreach (DataRow row in table.Rows)
+    {
+      T item = new T();
+      foreach (var prop in props)
+      {
+        if (table.Columns.Contains(prop.Name) && row[prop.Name] != DBNull.Value)
+        {
+          prop.SetValue(item, Convert.ChangeType(row[prop.Name], prop.PropertyType));
+        }
+      }
+      yield return item;
+    }
+  }
+}
---
+++ sample/converting-datatable-and-ienumerable/src/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace ConvertingDataTableAndIEnumerable;
+
+public record class Person
+{
+  public int Id { get; init; } = 0;
+  public string Name { get; init; } = string.Empty;
+  public int Age { get; init; } = 0;
+
+  // Genericで扱うためには引数なしパラメーターが必要っぽい。
+  // DataTableExtensionsでReflection使って引数渡しでインスタンス生成しようとしたけど、CS0310のエラーが消えなかった。
+  public Person() { }
+
+  public Person(int id, string name, int age)
+  {
+    Id = id;
+    Name = name;
+    Age = age;
+  }
+}
