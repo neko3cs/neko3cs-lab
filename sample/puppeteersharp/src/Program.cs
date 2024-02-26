d8accdd82afedb6f9b56095c946b51e850b88b51
neko3cs <minaton.0722.net@gmail.com>
2024-02-26 18:47:44 +0900

    feature: make screenshot sample app

---
+++ sample/puppeteersharp/src/Program.cs
@@ -0,0 +1,10 @@
+﻿using PuppeteerSharp;
+
+using var browserFetcher = new BrowserFetcher();
+await browserFetcher.DownloadAsync();
+await using var browser = await Puppeteer.LaunchAsync(
+  new LaunchOptions { Headless = true }
+);
+await using var page = await browser.NewPageAsync();
+await page.GoToAsync("http://www.google.com");
+await page.ScreenshotAsync("screenshot.jpg");
