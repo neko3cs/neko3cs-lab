f12d7cd10cb8137d913b6a7e18ad3d7722ae8227
neko3cs <minaton.0722.net@gmail.com>
2024-02-27 09:26:04 +0900

    feature: 以前作ったnodejs-puppeteerの処理が再現できるか確認

--- sample/puppeteersharp/src/Program.cs
+++ sample/puppeteersharp/src/Program.cs
@@ -6,5 +6,16 @@ await using var browser = await Puppeteer.LaunchAsync(
   new LaunchOptions { Headless = true }
 );
 await using var page = await browser.NewPageAsync();
-await page.GoToAsync("http://www.google.com");
+await page.GoToAsync("https://cgi-lib.berkeley.edu/ex/fup.html");
+
+File.WriteAllText(path: "text.txt", contents: "hoge");
+var waitTask = page.WaitForFileChooserAsync();
+await Task.WhenAll(
+  waitTask,
+  page.ClickAsync("input[type='file'][name='upfile']")
+);
+await waitTask.Result.AcceptAsync("text.txt");
+await page.TypeAsync("input[type='text'][name='note']", "fuga");
+
+await page.ClickAsync("input[type='submit'][value='Press']");
 await page.ScreenshotAsync("screenshot.jpg");
