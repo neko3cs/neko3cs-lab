25255e75a0d1dbce65971d5ca73410f67ca2a02c
neko3cs <minaton.0722.net@gmail.com>
2024-02-27 11:31:10 +0900

    【WIP】feature: インストール済みChromeでの起動

--- sample/puppeteersharp/src/Program.cs
+++ sample/puppeteersharp/src/Program.cs
@@ -1,12 +1,33 @@
 ﻿using PuppeteerSharp;
 
-using var browserFetcher = new BrowserFetcher();
-await browserFetcher.DownloadAsync();
+const string chromePath = @"/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome";
+
+var launchOptions = new LaunchOptions() { Headless = true };
+if (!File.Exists(chromePath))
+{
+#if DEBUG
+  Console.WriteLine("DEBUG: Cannot find chrome! Donwloading chromium...");
+#endif
+  using var browserFetcher = new BrowserFetcher();
+  await browserFetcher.DownloadAsync();
+  launchOptions.ExecutablePath = chromePath;
+}
+else
+{
+#if DEBUG
+  Console.WriteLine("DEBUG: Finded chrome! Don't donwload chromium.");
+#endif
+}
+
 await using var browser = await Puppeteer.LaunchAsync(
   new LaunchOptions { Headless = true }
 );
 await using var page = await browser.NewPageAsync();
 await page.GoToAsync("https://cgi-lib.berkeley.edu/ex/fup.html");
+
+#if DEBUG
+Console.WriteLine("DEBUG: Sent page.");
+#endif
 
 File.WriteAllText(path: "text.txt", contents: "hoge");
 var waitTask = page.WaitForFileChooserAsync();
@@ -17,5 +38,15 @@ await Task.WhenAll(
 await waitTask.Result.AcceptAsync("text.txt");
 await page.TypeAsync("input[type='text'][name='note']", "fuga");
 
+#if DEBUG
+Console.WriteLine("DEBUG: Uploaded file.");
+#endif
+
 await page.ClickAsync("input[type='submit'][value='Press']");
+await page.WaitForNavigationAsync();
+
 await page.ScreenshotAsync("screenshot.jpg");
+
+#if DEBUG
+Console.WriteLine("DEBUG: Navigated and took a screenshot.");
+#endif
