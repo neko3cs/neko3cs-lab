97d4f82be81987ed41656394bb6ef049baf31418
neko3cs <minaton.0722.net@gmail.com>
2024-02-29 14:59:45 +0900

    feature: mslearnのサンプルコードをとりあえず実装

--- sample/ef-core-code-first/README.md
+++ sample/ef-core-code-first/README.md
@@ -2,6 +2,52 @@
 
 EF Coreのコードファースト実装のサンプルコードです。
 
+## EF Core導入方法
+
+以下のコマンドでパッケージを導入する。
+
+```sh
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+dotnet add package Microsoft.EntityFrameworkCore.Design
+```
+
+また、CLIツールも導入すると便利。
+
+```sh
+dotnet tool install --global dotnet-ef
+```
+
+## サンプルアプリ実行方法
+
+データベースを用意する。
+
+dockerのSQL Server on Linuxを使った場合は以下の通り。
+
+```sh
+docker pull mcr.microsoft.com/mssql/server:latest &&
+docker run `
+  -e 'ACCEPT_EULA=Y' `
+  -e 'SA_PASSWORD=P@ssword!' `
+  -p 1433:1433 `
+  --name sql1 `
+  -v ${PWD}:/tmp `
+  -d mcr.microsoft.com/mssql/server:latest
+```
+
+任意のクライアントから以下のSQL文を実行する。
+
+```sql
+drop database if exists [EfCoreCodeFirst]
+create database [EfCoreCodeFirst]
+```
+
+プログラムを実行する。
+
+要所要所で `Console.ReadLine()` しているので、SQLを発行してDBの状態を見るとよろし。
+
 ## 参考資料
 
+- [概要 - EF Core | Microsoft Learn](https://learn.microsoft.com/ja-jp/ef/core/get-started/overview/first-app?tabs=netcore-cli)
+- [Entiy Framework Core のインストール - EF Core | Microsoft Learn](https://learn.microsoft.com/ja-jp/ef/core/get-started/overview/install)
 - [Create API と Drop API - EF Core | Microsoft Learn](https://learn.microsoft.com/ja-jp/ef/core/managing-schemas/ensure-created)
+
---
+++ sample/ef-core-code-first/src/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+using EfCoreCodeFirst;
+using EfCoreCodeFirst.Entity;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+var connectionString = new SqlConnectionStringBuilder
+{
+  DataSource = "localhost",
+  InitialCatalog = "EfCoreCodeFirst",
+  UserID = "sa",
+  Password = "P@ssword!",
+  TrustServerCertificate = true
+}.ConnectionString;
+
+using var db = new DatabaseContext(connectionString);
+if (await db.Database.EnsureCreatedAsync())
+{
+  Console.WriteLine("DEBUG: Created database.");
+  Console.ReadLine();
+}
+else
+{
+  // db.Database.EnsureCreated()とは同時に実行出来ないらしい
+  // URL: https://learn.microsoft.com/ja-jp/ef/core/managing-schemas/ensure-created
+  await db.Database.MigrateAsync();
+  Console.WriteLine("DEBUG: Migrated database.");
+}
+
+db.Blogs.Add(new Blog { Url = "http://neko3cs.net" });
+await db.SaveChangesAsync();
+
+Console.WriteLine("DEBUG: Inserted [Blogs] item.");
+Console.ReadLine();
+
+var blog = db.Blogs
+  .OrderBy(b => b.BlogId)
+  .First();
+Console.WriteLine($"DEBUG: Selected [Blogs]. item: {JsonSerializer.Serialize(blog)}");
+Console.ReadLine();
+
+blog.Url = "https://neko3cs.net/entry";
+blog.Posts.Add(new Post
+{
+  Title = "Hello, World!",
+  Content = "I wrote an app using EF Core!",
+});
+await db.SaveChangesAsync();
+
+Console.WriteLine("DEBUG: Changed [Blogs] item and insert [Posts] item.");
+Console.ReadLine();
+
+db.Remove(blog);
+await db.SaveChangesAsync();
+
+Console.WriteLine("DEBUG: Deleted [Blogs] and [Posts] items.");
