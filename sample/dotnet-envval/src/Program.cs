4be05c27a2a544306cade558217c157f91ef9bc7
neko3cs <minaton.0722.net@gmail.com>
2024-03-09 11:38:22 +0900

    feature: .NETの環境変数に関する取り扱いのサンプルコード

---
+++ sample/dotnet-envval/src/Program.cs
@@ -0,0 +1,10 @@
+﻿var value = Environment.GetEnvironmentVariable("DotnetEnvVal.Test", EnvironmentVariableTarget.User);
+if (string.IsNullOrEmpty(value))
+{
+    Environment.SetEnvironmentVariable("DotnetEnvVal.Test", "test value", EnvironmentVariableTarget.User);
+    Console.WriteLine("環境変数を設定しました。");
+}
+else
+{
+    Console.WriteLine($"環境変数を取得しました。: {value}");
+}
