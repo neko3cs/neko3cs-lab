a8175a80a272ab3860a0d16525365e7c60d720d2
neko3cs <minaton.0722.net@gmail.com>
2024-03-15 10:58:30 +0900

    refactor: app.configとapp.routeはmain.tsで書いちゃう

--- practice/ng-after-tutorial/src/main.ts
+++ practice/ng-after-tutorial/src/main.ts
@@ -1,6 +1,25 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { ApplicationConfig } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, provideRouter } from '@angular/router';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { UserDetailPageComponent } from './app/user-detail-page/user-detail-page.component';
+
+const routes: Routes = [
+  { path: 'users/:userId', component: UserDetailPageComponent }
+];
+const appConfig: ApplicationConfig = {
+  providers: [
+    CommonModule,
+    BrowserModule,
+    ReactiveFormsModule,
+    provideHttpClient(),
+    provideRouter(routes),
+  ]
+};
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
