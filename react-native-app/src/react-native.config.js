4925522eef843cbb4297d1955208bf6972e13df9
neko3cs <minaton.0722.net@gmail.com>
2024-03-23 22:04:35 +0900

    チェックボックスのアイコンが？になってた不具合の修正

---
+++ react-native-app/src/react-native.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: [
+    './node_modules/react-native-vector-icons/Fonts/'
+  ],
+  iosAssets: [],
+  androidAssets: [],
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
+      },
+    },
+  },
+};
