512a25ffe89c2fc658c7c9953a1cc585b9f89e5e
neko3cs <36363130+neko3cs@users.noreply.github.com>
2024-04-01 20:51:33 +0900

    macOSだとipコマンドがないので実行しない (#16)
    macOSなら別に表示しなくてもいいや

--- mssql-database/adventure-works-lt2022/setup_database.sh
+++ mssql-database/adventure-works-lt2022/setup_database.sh
@@ -9,5 +9,7 @@ sqlcmd \
   -S localhost -d master -U sa -P P@ssword! \
   -i ./RestoreDatabase.sql
 
-WSL_IP_ADDRESS=$(ip a | grep eth0 | grep inet | grep -oP '(\d{1,3}\.){3}\d{1,3}' | head -n 1)
-echo "If you are running docker on wsl2, use this ip address for SSMS.: $WSL_IP_ADDRESS"
+if echo $(uname -a) | grep -q '^Linux'; then
+  WSL_IP_ADDRESS=$(ip a | grep eth0 | grep inet | grep -oP '(\d{1,3}\.){3}\d{1,3}' | head -n 1)
+  echo "If you are running docker on wsl2, use this ip address for SSMS.: $WSL_IP_ADDRESS"
+fi
