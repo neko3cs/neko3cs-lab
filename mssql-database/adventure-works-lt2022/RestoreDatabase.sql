8714b86eb18e6f4c09928818ae34b88297f5cf08
neko3cs <36363130+neko3cs@users.noreply.github.com>
2024-04-01 13:55:29 +0900

    AdventureWorksの追加 (#13)

---
+++ mssql-database/adventure-works-lt2022/RestoreDatabase.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS [AdventureWorksLT2022];
+GO
+
+RESTORE DATABASE [AdventureWorksLT2022]
+FROM DISK = N'/data/AdventureWorksLT2022.bak'
+WITH
+  MOVE 'AdventureWorksLT2022_Data' TO '/var/opt/mssql/data/AdventureWorksLT2022.mdf',
+  MOVE 'AdventureWorksLT2022_Log' TO '/var/opt/mssql/data/AdventureWorksLT2022_log.ldf',
+  FILE = 1,
+  NOUNLOAD,
+  STATS = 5;
+GO
