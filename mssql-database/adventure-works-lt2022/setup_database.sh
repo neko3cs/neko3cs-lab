047c401b6a6eba8d2a40407b47b903d829add4f7
neko3cs <minaton.0722.net@gmail.com>
2024-04-07 18:14:16 +0900

    docker-compose upが少し時間がかかるので待ってあげる

--- mssql-database/adventure-works-lt2022/setup_database.sh
+++ mssql-database/adventure-works-lt2022/setup_database.sh
@@ -1,10 +1,24 @@
-#!/bin/sh
+#!/bin/sh -e
+
+function show_spinner {
+  PID=$1
+  DONE_MESSAGE=$2
+  SP='\|/-'
+  while ps -p $PID > /dev/null
+  do
+    printf "\b%c" "${SP:i++%4:1}"
+    sleep 0.1
+  done
+  printf "\n$DONE_MESSAGE\n"
+}
 
 if [ ! -f $PWD/AdventureWorksLT2022.bak ]; then 
   curl -fsSOL https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorksLT2022.bak
 fi
 
 docker-compose up --detach
+sleep 20 & pid=$!
+show_spinner $pid 'docker-compose up Done!'
 sqlcmd \
   -S localhost -d master -U sa -P P@ssword! \
   -i ./RestoreDatabase.sql
