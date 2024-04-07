7e54c311a7859e63874b29bac81b5fc7f46631f0
neko3cs <minaton.0722.net@gmail.com>
2024-04-07 18:55:05 +0900

    mssql-database: macOSでの不具合の修正 (#21)
    * docker-compose upが少し時間がかかるので待ってあげる
    
    * 注意書きの追加

--- mssql-database/README.md
+++ mssql-database/README.md
@@ -1,3 +1,8 @@
 # SQL Server on Linuxサンプル
 
 SQL Server on Linuxのサンプルデータベースです。
+
+## adventure-works-lt2022についてのメモ
+
+- メインのスキーマは `[SalesLT]`
+  - `[dbo]` を見て「あれ？テーブルみれない？！」とかならないように気をつける
--- mssql-database/neko3cs-database/setup_database.sh
+++ mssql-database/neko3cs-database/setup_database.sh
@@ -1,6 +1,20 @@
-#!/bin/sh
+#!/bin/sh -e
+
+function show_spinner {
+  PID=$1
+  DONE_MESSAGE=$2
+  SP='\|/-'
+  while ps -p $PID > /dev/null
+  do
+    printf "\b%c" "${SP:i++%4:1}"
+    sleep 0.1
+  done
+  printf "\n$DONE_MESSAGE\n"
+}
 
 docker-compose up --detach
+sleep 15 & pid=$!
+show_spinner $pid 'docker-compose up Done!'
 sqlcmd \
   -S 'localhost' -d 'master' -U 'sa' -P "P@ssword!" \
   -Q 'DROP DATABASE IF EXISTS [Neko3csDatabase]; CREATE DATABASE [Neko3csDatabase];'
