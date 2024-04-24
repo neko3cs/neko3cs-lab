c6c4fded161665aa299af9027927a2a52f6be1aa
neko3cs <minaton.0722.net@gmail.com>
2024-04-24 11:04:18 +0900

    TODOの追記

--- azure-virtual-machine-iac/src/Install-JapaneseLanguagePack.ps1
+++ azure-virtual-machine-iac/src/Install-JapaneseLanguagePack.ps1
@@ -1,8 +1,8 @@
 #Requires -PSEdition Desktop
 
+# TODO: 2019, 2022にも対応する
 $LanguagePackUrl = "http://download.windowsupdate.com/c/msdownload/update/software/updt/2016/09/lp_9a666295ebc1052c4c5ffbfa18368dfddebcd69a.cab"
 $LanguagePackFilePath = "$PWD\LanguagePackFile.cab"
-$InputTipJapanese = "0411:00000411"
 
 Set-WinUserLanguageList `
   -LanguageList ja-JP, en-US `
@@ -15,7 +15,7 @@ Add-WindowsPackage `
   -PackagePath $LanguagePackFilePath `
   -Online
 Set-WinDefaultInputMethodOverride `
-  -InputTip $InputTipJapanese
+  -InputTip "0411:00000411"
 Set-WinLanguageBarOption `
   -UseLegacySwitchMode `
   -UseLegacyLanguageBar
