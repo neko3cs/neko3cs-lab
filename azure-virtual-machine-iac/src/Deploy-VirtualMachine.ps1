a8b2248ca969aa5eb990fe07a9cf4bf80e439784
neko3cs <minaton.0722.net@gmail.com>
2024-04-24 15:09:54 +0900

    初期セットアップスクリプトを実行する

--- azure-virtual-machine-iac/src/Deploy-VirtualMachine.ps1
+++ azure-virtual-machine-iac/src/Deploy-VirtualMachine.ps1
@@ -6,11 +6,16 @@ $ResourceGroup = $EnvVal.RESOURCE_GROUP
 $Location = $EnvVal.LOCATION
 $AdminUserName = $EnvVal.ADMIN_USERNAME
 $AdminPassword = $EnvVal.ADMIN_PASSWORD
-$AllowedIpAddress = $EnvVal.ALLOWED_IPADDRESS
 $OSVersion = $EnvVal.OS_VERSION
 $VMName = $EnvVal.VM_NAME
 $VMSize = $EnvVal.VM_SIZE
 $DiskSizeGB = $EnvVal.DISK_SIZE_GB
+$AllowedIpAddress = (Invoke-RestMethod checkip.amazonaws.com)
+$initializeScripts = @(
+  'Enable-PingFirewallRule.ps1'
+  'Install-JapaneseLanguagePack.ps1'
+  'Set-JapaneseLanguageCulture.ps1'
+)
 
 az group create `
   --name $ResourceGroup `
@@ -24,6 +29,7 @@ az deployment group create `
   adminUsername="$AdminUserName" `
   adminPassword="$AdminPassword" `
   allowedIpAddress="$AllowedIpAddress" `
+  vmName="$VMName" `
   OSVersion="$OSVersion" `
   vmName="$VMName" `
   vmSize="$VMSize" `
@@ -34,6 +40,35 @@ az deployment group create `
   --resource-group $ResourceGroup `
   --name CreateWindowsServerVirtualMachine `
   --query properties.outputs.hostname.value).Trim('"') |
-Out-File -FilePath hostname.txt -Encoding utf8
+Out-File -FilePath hostname.txt -Encoding utf8 -NoNewline
 
-# TODO: 必要なスクリプトファイルをVMにアップロードし、実行する
+$hostName = Get-Content hostname.txt
+ssh-keygen -R $hostName
+az vm run-command invoke `
+  --resource-group $ResourceGroup `
+  --name $VMName `
+  --command-id RunPowerShellScript `
+  --scripts 'mkdir C:\Setup\'
+foreach ($script in $initializeScripts) {
+  scp "./$script" "$($AdminUserName)@$($hostName):/Setup/" # HACK: scpコマンドではパスワードが対話形式で聞かれてしまうので回避策を考える
+  $status = 'Running'
+  while ($status -ne 'Succeeded') {
+    $status = (az vm get-instance-view `
+        --resource-group $ResourceGroup `
+        --name $VMName `
+        --query instanceView.statuses[1].displayStatus `
+        --output tsv).Trim('"')
+    if ($status -ne 'VM running') {
+      Write-Output 'Wait for VM to restart...'
+      Start-Sleep -Seconds 10
+    }
+    else {
+      $status = 'Succeeded'
+    }
+  }
+  az vm run-command invoke `
+    --resource-group $ResourceGroup `
+    --name $VMName `
+    --command-id RunPowerShellScript `
+    --scripts ". C:\Setup\$script" # FIXME: 'Install-JapaneseLanguagePack.ps1'が乙る
+}
--- azure-virtual-machine-iac/src/Install-JapaneseLanguagePack.ps1
+++ azure-virtual-machine-iac/src/Install-JapaneseLanguagePack.ps1
@@ -1,6 +1,7 @@
 #Requires -PSEdition Desktop
 
-# TODO: 2019, 2022にも対応する
+# TODO: LanguagePackManagementモジュールを使って2019, 2022にも対応する。
+# URL: https://jpwinsup.github.io/blog/2023/03/06/UserInterfaceAndApps/LanguageSupport_IME/InstallLanguage/
 $LanguagePackUrl = "http://download.windowsupdate.com/c/msdownload/update/software/updt/2016/09/lp_9a666295ebc1052c4c5ffbfa18368dfddebcd69a.cab"
 $LanguagePackFilePath = "$PWD\LanguagePackFile.cab"
 
