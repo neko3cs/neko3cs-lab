d54fa9988601f672961326a78083931400172bae
neko3cs <minaton.0722.net@gmail.com>
2024-05-11 23:49:24 +0900

    リネーム：dotnet-maui-blazor -> aspnet-core-blazor

---
+++ aspnet-core-blazor/src/AspNetCoreBlazor.Maui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCoreBlazor.Maui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ aspnet-core-blazor/src/AspNetCoreBlazor.Maui/MauiProgram.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using AspNetCoreBlazor.Maui.Data;
+
+namespace AspNetCoreBlazor.Maui;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp() => MauiApp.CreateBuilder()
+		.UseMauiApp<App>()
+		.ConfigureFonts(fonts =>
+		{
+			fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+		})
+		.ConfigureServices(services =>
+		{
+			services.AddMauiBlazorWebView();
+			services.AddSingleton<WeatherForecastService>();
+#if DEBUG
+			services.AddBlazorWebViewDeveloperTools();
+#endif
+		})
+#if DEBUG
+		.ConfigureLogging(builder =>
+		{
+			builder.Logging.AddDebug();
+		})
+#endif
+		.Build();
+
+	#region HelperMethod
+	private static MauiAppBuilder ConfigureServices(this MauiAppBuilder builder, Action<IServiceCollection> action)
+	{
+		action(builder.Services);
+		return builder;
+	}
+
+	private static MauiAppBuilder ConfigureLogging(this MauiAppBuilder builder, Action<MauiAppBuilder> action)
+	{
+		action(builder);
+		return builder;
+	}
+	#endregion
+}
