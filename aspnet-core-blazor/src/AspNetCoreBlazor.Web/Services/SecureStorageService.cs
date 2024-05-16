98eb5060c2c2dbba30896c02cb4d67dfe71e72d6
neko3cs <minaton.0722.net@gmail.com>
2024-05-16 13:43:37 +0900

    メモ追記

--- aspnet-core-blazor/src/AspNetCoreBlazor.Web/Services/SecureStorageService.cs
+++ aspnet-core-blazor/src/AspNetCoreBlazor.Web/Services/SecureStorageService.cs
@@ -3,6 +3,7 @@ using AspNetCoreBlazor.Core.Types;
 
 namespace AspNetCoreBlazor.Web.Services;
 
+// HACK: ページ遷移時先にDLL読んでるのか実装がないとエラーしてしまうので一旦用意 -> 実装しなくても回避する方法があればそちらにする
 public class SecureStorageService : ISecureStorageService
 {
     public Task<User> GetCurrentUserAsync()
