83b6104500d5be6071b7204d44dcca5a1b7c381a
neko3cs <minaton.0722.net@gmail.com>
2024-05-16 15:56:15 +0900

    Blazor化

--- aspnet-core-blazor/src/AspNetCoreBlazor.Wpf/App.xaml.cs
+++ aspnet-core-blazor/src/AspNetCoreBlazor.Wpf/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace AspNetCoreBlazor.Wpf;
 
@@ -10,4 +8,3 @@ namespace AspNetCoreBlazor.Wpf;
 public partial class App : Application
 {
 }
-
--- aspnet-core-blazor/src/AspNetCoreBlazor.Wpf/MainWindow.xaml.cs
+++ aspnet-core-blazor/src/AspNetCoreBlazor.Wpf/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AspNetCoreBlazor.Wpf;
 
@@ -19,5 +11,9 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddWpfBlazorWebView();
+        Resources.Add("services", serviceCollection.BuildServiceProvider());
     }
 }
