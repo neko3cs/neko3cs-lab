ce1ad14f7932e6b39ddaaa751993e3ce955816ce
neko3cs <minaton.0722.net@gmail.com>
2024-05-22 20:03:17 +0900

    fluentui-blazorの導入

--- aspnet-core-blazor/src/AspNetCoreBlazor.Web/Program.cs
+++ aspnet-core-blazor/src/AspNetCoreBlazor.Web/Program.cs
@@ -4,11 +4,13 @@ using AspNetCoreBlazor.Web;
 using AspNetCoreBlazor.Web.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddFluentUIComponents();
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton(_ => new PlatformDetection(PlatformKind.Browser));
 builder.Services.AddSingleton<IUserService, UserService>();
