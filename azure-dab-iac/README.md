13604ba7f4aa910971b3243cb48fd3eb0d0d950b
neko3cs <minaton.0722.net@gmail.com>
2024-05-24 19:36:14 +0900

    デプロイコードの作成

--- azure-dab-iac/README.md
+++ azure-dab-iac/README.md
@@ -2,3 +2,13 @@
 
 Azure Data API BuilderをBicepでデプロイするIaCコードです。
 
+## .envファイルサンプル
+
+```txt
+LOCATION=japaneast
+RESOURCE_GROUP=rg-sample-app
+APP_NAME=sample-app
+SQL_DATABASE_NAME=SampleAppDatabase
+SQL_SERVER_LOGIN=db_admin
+SQL_SERVER_LOGIN_PASSWORD=P@ssword!
+```
---
+++ azure-dab-iac/src/CreateTable.sql
@@ -0,0 +1,32 @@
+USE [SampleAppDatabase]
+
+SET ANSI_NULLS ON
+
+DROP TABLE IF EXISTS [data]
+CREATE TABLE [data]
+(
+	[id] [int] NOT NULL IDENTITY(1,1),
+	[value] [nvarchar](max) NULL,
+	[created_at] [datetime2](7) NOT NULL default getdate(),
+	[updated_at] [datetime2](7) NOT NULL default getdate(),
+	CONSTRAINT [PK_data] PRIMARY KEY CLUSTERED ([id] ASC) WITH (
+		STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF
+	) ON [PRIMARY]
+)
+ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+DROP TRIGGER IF EXISTS [trg_data_UpdateUpdatedAt]
+GO
+
+CREATE TRIGGER [trg_data_UpdateUpdatedAt] ON [data]
+AFTER UPDATE AS
+BEGIN
+	IF (update([id]) or update([value]) or update([created_at]))
+	BEGIN
+		UPDATE	[data]
+		SET			[updated_at] = GETDATE()
+		FROM		[data]
+			INNER JOIN [inserted]
+			ON		[data].[id] = [inserted].[id]
+	END
+END
