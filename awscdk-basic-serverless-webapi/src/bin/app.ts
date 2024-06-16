ac185eb57bc7ae92e42dddb707b1156598139c08
neko3cs <minaton.0722.net@gmail.com>
2024-06-16 13:41:37 +0900

    ひとまず、シンプルなApacheアプリから始める

--- awscdk-basic-serverless-webapi/src/bin/app.ts
+++ awscdk-basic-serverless-webapi/src/bin/app.ts
@@ -1,12 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import * as dotenv from 'dotenv';
 import { AppStack } from '../lib/app-stack';
 
-dotenv.config();
 const app = new cdk.App();
-new AppStack(app, 'AppStack', {
-  ServicePrefix: process.env.SERVICE_PREFIX ?? 'App',
-  MyIPAddress: process.env.MY_IP_ADDRESS ?? '*',
-});
+new AppStack(app, 'AppStack');
