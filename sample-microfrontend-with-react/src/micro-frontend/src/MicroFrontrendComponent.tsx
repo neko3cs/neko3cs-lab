da432f410077889ea20048306ed80890e2da44ec
neko3cs <minaton.0722.net@gmail.com>
2024-12-08 22:04:15 +0900

    なんかちゃんと分離出来てなかったので、マイクロフロントエンド側に描画処理追加

--- sample-microfrontend-with-react/src/micro-frontend/src/MicroFrontrendComponent.tsx
+++ sample-microfrontend-with-react/src/micro-frontend/src/MicroFrontrendComponent.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Text } from '@fluentui/react';
 
+const titleStyle = {
+  root: {
+    marginBottom: '20px'
+  }
+};
 const textStyle = {
   root: {
     textAlign: 'center',
@@ -10,9 +15,16 @@ const textStyle = {
 
 const MicroFrontrendComponent: React.FC = () => {
   return (
-    <Text variant="large" styles={textStyle}>
-      これはマイクロフロントエンドのコンポーネントです。
-    </Text>
+    <div>
+      <Text
+        variant='xxLarge'
+        styles={titleStyle}>
+        Micro Frontend
+      </Text>
+      <Text variant="large" styles={textStyle}>
+        これはマイクロフロントエンドのコンポーネントです。
+      </Text>
+    </div>
   );
 };
 
