bc0b1b58b275769b62edc628ed784de9478bc9a4
neko3cs <minaton.0722.net@gmail.com>
2024-12-08 18:31:14 +0900

    見た目をちょっと綺麗にしてみる

--- sample-microfrontend-with-react/src/micro-frontend/src/MicroFrontrendComponent.tsx
+++ sample-microfrontend-with-react/src/micro-frontend/src/MicroFrontrendComponent.tsx
@@ -1,7 +1,19 @@
-const MicroFrontrendComponent = () => {
-  return <div>
-    これはマイクロフロントエンドのコンポーネントです。
-  </div>;
+import React from 'react';
+import { Text } from '@fluentui/react';
+
+const textStyle = {
+  root: {
+    textAlign: 'center',
+    color: '#333'
+  }
+};
+
+const MicroFrontrendComponent: React.FC = () => {
+  return (
+    <Text variant="large" styles={textStyle}>
+      これはマイクロフロントエンドのコンポーネントです。
+    </Text>
+  );
 };
 
 export default MicroFrontrendComponent;
