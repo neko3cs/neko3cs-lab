9b7c6e0c5eaeff9c834ddf6744ef72774e0db886
neko3cs <minaton.0722.net@gmail.com>
2024-12-08 18:31:14 +0900

    見た目をちょっと綺麗にしてみる

--- sample-microfrontend-with-react/src/app-shell/src/App.tsx
+++ sample-microfrontend-with-react/src/app-shell/src/App.tsx
@@ -1,19 +1,86 @@
 import React from 'react';
+import styled from 'styled-components';
+import { DefaultButton, Spinner, SpinnerSize, Text } from '@fluentui/react';
 import { useMicroFrontend } from './hooks/useMicroFrontend';
 
-const App = () => {
+const Layout = styled.div`
+  display: grid;
+  grid-template-rows: 60px 1fr;
+  grid-template-columns: 240px 1fr;
+  height: 100vh;
+  grid-template-areas:
+    'header header'
+    'sidebar content';
+`;
+const Header = styled.header`
+  grid-area: header;
+  background-color: #0078d4;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 20px;
+`;
+const Sidebar = styled.aside`
+  grid-area: sidebar;
+  background-color: rgba(255, 255, 255, 0.5);
+  backdrop-filter: blur(10px);
+  box-shadow: 2px 0px 5px rgba(0, 0, 0, 0.2);
+  padding: 20px;
+`;
+const ContentArea = styled.main`
+  grid-area: content;
+  background-color: white;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+`;
+const buttonStyle = {
+  root: {
+    margin: '10px 0'
+  }
+};
+const textStyle = {
+  root: {
+    marginBottom: '20px'
+  }
+};
+
+const App: React.FC = () => {
   const MicroFrontrendComponent = useMicroFrontend(
     'http://localhost:5050/MicroFrontrendComponent.js',
     'MicroFrontrendComponent'
   );
 
   return (
-    <div>
-      <h1>ホストアプリケーション</h1>
-      <div>
-        {MicroFrontrendComponent ? <MicroFrontrendComponent /> : <p>Loading MicroFrontrendComponent...</p>}
-      </div>
-    </div>
+    <Layout>
+      <Header>
+        ホストアプリケーション
+      </Header>
+
+      <Sidebar>
+        <Text variant="large">
+          Menu
+        </Text>
+        <DefaultButton text="Page1" styles={buttonStyle} />
+        <DefaultButton text="リンク2" styles={buttonStyle} />
+      </Sidebar>
+
+      <ContentArea>
+        <Text
+          variant='xxLarge'
+          styles={textStyle}>
+          Micro Frontend
+        </Text>
+        {MicroFrontrendComponent ? (
+          <MicroFrontrendComponent />
+        ) : (
+          <Spinner size={SpinnerSize.large} label='Loading...' />
+        )}
+      </ContentArea>
+    </Layout>
   );
 };
 
