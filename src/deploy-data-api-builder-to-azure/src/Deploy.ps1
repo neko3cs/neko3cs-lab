c34a20ce6f177ba2903e2544e4c620c2a7777215
neko3cs <minaton.0722.net@gmail.com>
2025-01-06 17:50:57 +0900

    デプロイ名をMicroSoft参考にする

--- src/deploy-data-api-builder-to-azure/src/Deploy.ps1
+++ src/deploy-data-api-builder-to-azure/src/Deploy.ps1
@@ -4,32 +4,33 @@ $ErrorActionPreference = 'Stop'
 $ResourceGroup = "rg-sampledabapi-dev-japaneast-001"
 $Location = "japaneast"
 
-# az provider register --namespace "Microsoft.App"
-# az provider register --namespace "Microsoft.ContainerService"
-
 az group create `
   --name $ResourceGroup `
   --location $Location `
   --output table
 
+$DatabaseDeployName = "SampleDabApp.Database-$(Get-Date -Format 'yyyyMMddHHmmss')"
 az deployment group create `
-  --name "DatabaseDeploy" `
+  --name $DatabaseDeployName `
   --resource-group $ResourceGroup `
-  --template-file ./Database.bicep `
-  --parameters ./Development.bicepparam `
+  --template-file Database.bicep `
+  --parameters Development.bicepparam `
   --output table
 
 $connectionString = az deployment group show `
   --resource-group $ResourceGroup `
-  --name "DatabaseDeploy" `
+  --name $DatabaseDeployName `
   --query properties.outputs.connectionString.value
 
 Invoke-Sqlcmd `
   -ConnectionString $connectionString `
   -InputFile ./CreateTable.sql
 
+# az provider register --namespace "Microsoft.App"
+# az provider register --namespace "Microsoft.ContainerService"
 # az deployment group create `
+#   --name "SampleDabApp.Application-$(Get-Date -Format 'yyyyMMddHHmmss')" `
 #   --resource-group $ResourceGroup `
-#   --template-file ./Application.bicep `
-#   --parameters ./Application.bicepparam `
+#   --template-file Application.bicep `
+#   --parameters Development.bicepparam `
 #   --output table
