626ee95718c7ae1aa113107d44f63ec4383654db
neko3cs <minaton.0722.net@gmail.com>
2025-01-18 21:09:11 +0900

    コンソールアプリの実装

--- src/dotnet-9-console-app/src/Program.cs
+++ src/dotnet-9-console-app/src/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using GenericHostConsoleApp;
+
+var host = Host.CreateDefaultBuilder(args)
+  .ConfigureAppConfiguration((hostContext, config) =>
+  {
+    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+  })
+  .ConfigureServices((hostContext, services) =>
+  {
+    services.AddHostedService<Worker>();
+  })
+  .ConfigureLogging((hostContext, logging) =>
+  {
+    logging.AddConsole();
+  })
+  .Build();
+
+await host.RunAsync();
