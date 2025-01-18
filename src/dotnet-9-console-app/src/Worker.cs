369d97dff6b49c25820923b4010d626ac67e8f06
neko3cs <minaton.0722.net@gmail.com>
2025-01-18 21:09:11 +0900

    コンソールアプリの実装

---
+++ src/dotnet-9-console-app/src/Worker.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GenericHostConsoleApp;
+
+public class Worker : BackgroundService
+{
+  private readonly ILogger<Worker> _logger;
+  private readonly IConfiguration _configuration;
+  private readonly IHostApplicationLifetime _applicationLifetime;
+
+  public Worker(
+    ILogger<Worker> logger,
+    IConfiguration configuration,
+    IHostApplicationLifetime applicationLifetime
+  )
+  {
+    _logger = logger;
+    _configuration = configuration;
+    _applicationLifetime = applicationLifetime;
+  }
+
+  protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+  {
+    _logger.LogInformation("Worker starting at: {time}", DateTimeOffset.Now);
+
+    var appName = _configuration["Settings:ApplicationName"];
+    var version = _configuration["Settings:Version"];
+    var awaitAt_ms = int.Parse(_configuration["Settings:AwaitAt_ms"]);
+
+    _logger.LogInformation("Application Name: {AppName}, Version: {Version}", appName, version);
+    await Task.Delay(awaitAt_ms, cancellationToken);
+
+    _logger.LogInformation("Worker stopping at: {time}", DateTimeOffset.Now);
+
+    _applicationLifetime.StopApplication();
+  }
+}
