93329187df2a1e25d8eb0783819dcc252593d3e4
neko3cs <minaton.0722.net@gmail.com>
2025-02-02 22:45:26 +0900

    wip:minikube+podmanへ移行

--- src/mssql-database/adventure-works-lt2022/setup_database.sh
+++ src/mssql-database/adventure-works-lt2022/setup_database.sh
@@ -1,29 +1,8 @@
 #!/bin/sh -e
 
-function show_spinner {
-  PID=$1
-  DONE_MESSAGE=$2
-  SP='\|/-'
-  while ps -p $PID > /dev/null
-  do
-    printf "\b%c" "${SP:i++%4:1}"
-    sleep 0.1
-  done
-  printf "\n$DONE_MESSAGE\n"
-}
+kubectl apply -f adventureworks.yaml
 
-if [ ! -f $PWD/AdventureWorksLT2022.bak ]; then 
-  curl -fsSOL https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorksLT2022.bak
-fi
+minikube ssh -- "sudo chmod 777 /data && cd /data && curl -fsSOL https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorksLT2022.bak"
 
-docker-compose up --detach
-sleep 20 & pid=$!
-show_spinner $pid 'docker-compose up Done!'
-sqlcmd \
-  -S localhost -d master -U sa -P P@ssword! \
-  -i ./RestoreDatabase.sql
-
-if echo $(uname -a) | grep -q '^Linux'; then
-  WSL_IP_ADDRESS=$(ip a | grep eth0 | grep inet | grep -oP '(\d{1,3}\.){3}\d{1,3}' | head -n 1)
-  echo "If you are running docker on wsl2, use this ip address for SSMS.: $WSL_IP_ADDRESS"
-fi
+# TODO: unable to open tcp connection with host '192.168.49.2:1433': dial tcp 192.168.49.2:1433: i/o timeout
+sqlcmd -S "$(minikube ip)" -U sa -P P@ssword! -d master -i ./RestoreDatabase.sql
