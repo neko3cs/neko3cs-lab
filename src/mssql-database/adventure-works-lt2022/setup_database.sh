35c1e5d9041ce38057786de9c98e596fe210a819
neko3cs <minaton.0722.net@gmail.com>
2025-02-24 19:44:02 +0900

    podman単体での動作確認済み

--- src/mssql-database/adventure-works-lt2022/setup_database.sh
+++ src/mssql-database/adventure-works-lt2022/setup_database.sh
@@ -2,7 +2,7 @@
 
 kubectl apply -f adventureworks.yaml
 
-minikube ssh -- "sudo chmod 777 /data && cd /data && curl -fsSOL https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorksLT2022.bak"
+minikube ssh -- "cd /var/opt/mssql/data && wget --no-verbose --show-progress --https-only --output-document=AdventureWorksLT2022.bak https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorksLT2022.bak"
 
 # TODO: unable to open tcp connection with host '192.168.49.2:1433': dial tcp 192.168.49.2:1433: i/o timeout
 sqlcmd -S "$(minikube ip)" -U sa -P P@ssword! -d master -i ./RestoreDatabase.sql
