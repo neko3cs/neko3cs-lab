001891bed4ef2c60ae412cc8ca2942532c9522a1
Suzuki Minato (鈴木 湊) <Minato.Suzuki@jp.ricoh.com>
2025-02-25 15:02:44 +0900

    bakファイルをダウンロードする処理をyamlへ移動する

--- src/mssql-database/adventure-works-lt2022/setup_database.sh
+++ src/mssql-database/adventure-works-lt2022/setup_database.sh
@@ -2,7 +2,6 @@
 
 kubectl apply -f adventureworks.yaml
 
-minikube ssh -- "cd /var/opt/mssql/data && wget --no-verbose --show-progress --https-only --output-document=AdventureWorksLT2022.bak https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorksLT2022.bak"
+kubectl wait --for=condition=Ready pod -l app=adventureworks --timeout=60s
 
-# TODO: unable to open tcp connection with host '192.168.49.2:1433': dial tcp 192.168.49.2:1433: i/o timeout
-sqlcmd -S "$(minikube ip)" -U sa -P P@ssword! -d master -i ./RestoreDatabase.sql
+sqlcmd -S "$(minikube ip),31433" -U sa -P P@ssword! -d master -i ./RestoreDatabase.sql
