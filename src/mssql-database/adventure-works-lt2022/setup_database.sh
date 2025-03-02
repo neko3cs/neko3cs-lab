73e695e7977c836a5895bdc2bf3db9fa629deb87
neko3cs <minaton.0722.net@gmail.com>
2025-03-02 16:05:52 +0900

    もはやいらないので削除

--- src/mssql-database/adventure-works-lt2022/setup_database.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-kubectl apply -f adventureworks.yaml
-
-kubectl wait --for=condition=Ready pod -l app=adventureworks --timeout=60s
-
-sqlcmd -S "$(minikube ip),31433" -U sa -P P@ssword! -d master -i ./RestoreDatabase.sql
