16be785833ca60b6cd1d10ce651416bb56509840
neko3cs <minaton.0722.net@gmail.com>
2025-04-10 16:16:00 +0900

    AntDesign導入

--- src/nextjs-app-tutorial/src/app/layout.tsx
+++ src/nextjs-app-tutorial/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import 'antd/dist/reset.css';
 import "./globals.css";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <AntdRegistry>
+          {children}
+        </AntdRegistry>
       </body>
     </html>
   );
