87312efe8df15225b933f0c42f649a02f647da84
neko3cs <minaton.0722.net@gmail.com>
2025-12-24 23:12:22 +0900

    フォルダ構成の修正

--- src/ng-todo-app/src/app/components/app.ts
+++ src/ng-todo-app/src/app/components/app.ts
@@ -4,8 +4,12 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  styles: [],
+  template: `
+    <h1>Hello, {{ title() }}</h1>
+    <router-outlet></router-outlet>
+  `,
+  standalone: true,
 })
 export class App {
   protected readonly title = signal('ng-todo-app');
