9d75472b298dce427c610296f95667aa60eb5d09
neko3cs <minaton.0722.net@gmail.com>
2026-01-13 17:51:55 +0900

    追加機能の実装

--- ng-firestore/src/app/app.ts
+++ ng-firestore/src/app/app.ts
@@ -1,16 +1,60 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { MatCard } from '@angular/material/card';
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
+import { Firestore } from '@angular/fire/firestore';
+import { Person } from './person';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{ title() }}!</h1>
-
-    <router-outlet />
-  `,
+  imports: [MatCard, MatButtonModule, MatInputModule, MatIconModule, FormsModule, MatTableModule],
   styles: [],
+  template: `
+    <mat-card style="max-width: 400px; margin: 2rem auto; padding: 1rem;">
+      <h1>Angular Firestore Sample</h1>
+
+      <mat-form-field appearance="outline" style="width: 100%; margin-top: 1rem;">
+        <input matInput [value]="name()" placeholder="名前を入力" (input)="name.set($event.target.value)" />
+      </mat-form-field>
+      <mat-form-field appearance="outline" style="width: 100%; margin-top: 1rem;">
+        <input matInput [value]="age()" type="number" placeholder="年齢を入力"
+                (input)="age.set($any($event.target).valueAsNumber)" />
+      </mat-form-field>
+
+      <div style="text-align: center;">
+        <button mat-raised-button color="accent" (click)="add()" style="width: 100%;">
+          登録
+        </button>
+      </div>
+
+      <table mat-table [dataSource]="people()" class="mat-elevation-z8" style="width: 100%; margin-top: 1rem;">
+        <ng-container matColumnDef="Name">
+          <th mat-header-cell *matHeaderCellDef> 名前 </th>
+          <td mat-cell *matCellDef="let p"> {{ p.Name }} </td>
+        </ng-container>
+        <ng-container matColumnDef="Age">
+          <th mat-header-cell *matHeaderCellDef> 年齢 </th>
+          <td mat-cell *matCellDef="let p"> {{ p.Age }} </td>
+        </ng-container>
+
+        <tr mat-header-row *matHeaderRowDef="['Name', 'Age']"></tr>
+        <tr mat-row *matRowDef="let row; columns: ['Name', 'Age']"></tr>
+      </table>
+    </mat-card>
+  `,
 })
 export class App {
-  protected readonly title = signal('ng-firestore');
+  private firestore = inject(Firestore);
+
+  name = signal('');
+  age = signal(0);
+  people = signal<Person[]>([]);
+
+  add(): void {
+    const person: Person = { Id: crypto.randomUUID(), Name: this.name(), Age: this.age() };
+    this.people.update(list => [...list, person]);
+  }
 }
