3674020b868e35c38f6ae4897afeaa233b35d3c6
neko3cs <neko3cs@neko3cs-MacBook-Pro.local>
2026-02-05 20:09:22 +0900

    chore: clean up xmake.lua and remove redundant main in tests

--- hello_cpp_api/test/HelloApiTest.cpp
+++ hello_cpp_api/test/HelloApiTest.cpp
@@ -41,8 +41,3 @@ TEST_F(HelloApiTest, GetHello) {
     }
     EXPECT_TRUE(finished);
 }
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
