b539f526dfb3197843231d76145ca699e48e947a
neko3cs <neko3cs@neko3cs-MacBook-Pro.local>
2026-02-06 19:02:42 +0900

    feat: initial commit with hello world grpc

---
+++ go_grpc_server_client/src/client/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "example.com/hello-grpc/src/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: "World"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+}
