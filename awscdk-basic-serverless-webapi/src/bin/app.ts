11f9b6766cdb9f292b9e1094107d2a81c012d605
neko3cs <minaton.0722.net@gmail.com>
2026-02-16 16:29:29 +0900

    feat: implement port mapping (ALB:80 -> Fargate:3000) and switch to asset-based ECS image

--- awscdk-basic-serverless-webapi/src/bin/app.ts
+++ awscdk-basic-serverless-webapi/src/bin/app.ts
@@ -5,7 +5,7 @@ import { AppStack } from '../lib/app-stack';
 
 const app = new cdk.App();
 
-const appPort = process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 80;
+const appPort = process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 3000;
 const useNatGateway = process.env.USE_NAT_GATEWAY === 'true' || true;
 const acmArn = process.env.ACM_ARN;
 
