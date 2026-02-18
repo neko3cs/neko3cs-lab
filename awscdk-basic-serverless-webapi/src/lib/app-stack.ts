4099a4218fc6012f1f81cc8ad6e9066f4c5b3758
neko3cs <minaton.0722.net@gmail.com>
2026-02-18 18:38:17 +0900

    refactor: reorganize infrastructure into logical constructs and clean up comments

--- awscdk-basic-serverless-webapi/src/lib/app-stack.ts
+++ awscdk-basic-serverless-webapi/src/lib/app-stack.ts
@@ -1,108 +1,57 @@
 import * as cdk from 'aws-cdk-lib';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-
+import * as ecs from 'aws-cdk-lib/aws-ecs';
 import { Construct } from 'constructs';
 
-import { VpcSubnets } from './constructs/vpc-subnets';
-import { VpcEndpoints } from './constructs/vpc-endpoints';
-import { LoadBalancer } from './constructs/load-balancer';
-import { SecurityGroups } from './constructs/security-groups';
-import { RelationalDatabaseService } from './constructs/relational-database-service';
-import { FargateService } from './constructs/fargate-service';
+import { NetworkConstruct } from './network-construct';
+import { DatabaseConstruct } from './database-construct';
+import { ApplicationConstruct } from './application-construct';
 
-interface AppStackProps extends cdk.StackProps {
-  appPort: number;
-  useNatGateway?: boolean;
-  acmArn?: string;
-  apiAppContainerTag?: string;
-  pageAppContainerTag?: string;
-  apiAppRepositoryName?: string;
-  pageAppRepositoryName?: string;
+export interface AppStackProps extends cdk.StackProps {
+  readonly appPort: number;
+  readonly useNatGateway?: boolean;
+  readonly acmArn?: string;
 }
 
 export class AppStack extends cdk.Stack {
-  constructor(
-    scope: Construct,
-    id: string,
-    {
-      appPort,
-      useNatGateway = true,
-      acmArn,
-      apiAppContainerTag,
-      pageAppContainerTag,
-      apiAppRepositoryName,
-      pageAppRepositoryName,
-      ...props
-    }: AppStackProps
-  ) {
+  constructor(scope: Construct, id: string, props: AppStackProps) {
     super(scope, id, props);
 
     cdk.Tags.of(this).add('CdkStackName', 'Neko3csAppStack');
 
-    const { vpc, publicSubnets, appSubnets, dbSubnets } = new VpcSubnets(
-      this,
-      'VpcSubnets',
-      {
-        useNatGateway,
-      },
-    );
-
-    const { appSecurityGroup, dbSecurityGroup } = new SecurityGroups(
-      this,
-      'SecurityGroups',
-      {
-        vpc,
-      },
-    );
-
-    new VpcEndpoints(this, 'VpcEndpoints', {
-      vpc,
-      appSubnets,
-      appSecurityGroup,
+    const network = new NetworkConstruct(this, 'NetworkConstruct', {
+      useNatGateway: props.useNatGateway,
     });
 
-    const { apiTargetGroup, pageTargetGroup, loadBalancer } = new LoadBalancer(
-      this,
-      'LoadBalancer',
-      {
-        vpc,
-        vpcSubnets: publicSubnets,
-        appPort,
-        acmArn,
-      },
-    );
+    const database = new DatabaseConstruct(this, 'DatabaseConstruct', {
+      vpc: network.vpc,
+      subnets: network.dbSubnets,
+      securityGroup: network.dbSecurityGroup,
+      databaseName: 'app',
+    });
 
-    appSecurityGroup.addIngressRule(
-      loadBalancer.connections.securityGroups[0],
-      ec2.Port.tcp(appPort),
-      'Allow ALB to App',
-    );
+    const application = new ApplicationConstruct(this, 'ApplicationConstruct', {
+      vpc: network.vpc,
+      appSubnets: network.appSubnets,
+      publicSubnets: network.publicSubnets,
+      albSecurityGroup: network.albSecurityGroup,
+      appSecurityGroup: network.appSecurityGroup,
+      appPort: props.appPort,
+      acmArn: props.acmArn,
+    });
 
-    const { dbCluster } = new RelationalDatabaseService(
-      this,
-      'RelationalDatabaseService',
-      {
-        vpc,
-        vpcSubnets: dbSubnets,
-        securityGroup: dbSecurityGroup,
-      },
-    );
+    const container = application.taskDefinition.defaultContainer!;
 
-    dbSecurityGroup.addIngressRule(
-      appSecurityGroup,
-      ec2.Port.tcp(5432),
-    );
+    container.addEnvironment('DB_HOST', database.cluster.clusterEndpoint.hostname);
+    container.addEnvironment('DB_PORT', '5432');
+    container.addEnvironment('DB_NAME', 'app');
 
-    new FargateService(this, 'FargateService', {
-      vpc,
-      vpcSubnets: appSubnets,
-      securityGroup: appSecurityGroup,
-      dbCredential: dbCluster.secret!,
-      databaseHost: dbCluster.clusterEndpoint.hostname,
-      databasePort: dbCluster.clusterEndpoint.port,
-      containerPort: appPort,
-      apiTargetGroup,
-      pageTargetGroup,
-    }).node.addDependency(dbCluster);
+    container.addSecret('DB_PASSWORD', ecs.Secret.fromSecretsManager(database.cluster.secret!, 'password'));
+    container.addSecret('DB_USER', ecs.Secret.fromSecretsManager(database.cluster.secret!, 'username'));
+
+    // ネットワーク層で定義したメソッドを使い、各レイヤー間の通信を許可
+    network.addConnectivityRules(props.appPort);
+
+    // データベースの準備が整ってからアプリケーションを起動
+    application.service.node.addDependency(database.cluster);
   }
 }
