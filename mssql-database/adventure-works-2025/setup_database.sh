60ee78432e373d88d9ed8bbf0dfbed111a4d8fb4
neko3cs <minaton.0722.net@gmail.com>
2026-02-22 21:57:48 +0900

    add: mssql-database / adventure-works-2025

---
+++ mssql-database/adventure-works-2025/setup_database.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+if [ ! -f $PWD/AdventureWorks2025.bak ]; then 
+  curl -fsSOL https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2025.bak
+fi
+
+docker-compose up --detach
+sqlcmd \
+  -S localhost -d master -U sa -P P@ssword! \
+  -i ./RestoreDatabase.sql
+
+if echo $(uname -a) | grep -q '^Linux'; then
+  WSL_IP_ADDRESS=$(ip a | grep eth0 | grep inet | grep -oP '(\d{1,3}\.){3}\d{1,3}' | head -n 1)
+  echo "If you are running docker on wsl2, use this ip address for SSMS.: $WSL_IP_ADDRESS"
+fi
