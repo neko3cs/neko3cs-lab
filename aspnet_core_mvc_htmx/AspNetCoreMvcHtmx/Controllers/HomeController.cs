be04fa5cf4cbe29112f98c2745decd5cf2e14dfd
neko3cs <minaton.0722.net@gmail.com>
2026-03-02 02:20:37 +0900

    style: タイトルを更新し、不要な Privacy ページを削除

--- aspnet_core_mvc_htmx/AspNetCoreMvcHtmx/Controllers/HomeController.cs
+++ aspnet_core_mvc_htmx/AspNetCoreMvcHtmx/Controllers/HomeController.cs
@@ -31,11 +31,6 @@ public class HomeController : Controller
         return PartialView("_CitiesPartial", cities);
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
